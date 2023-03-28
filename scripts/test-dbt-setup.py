f093dc8ba0426b5dd18424092a52c241d4f8b479
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-03-28 08:10:05 -0500

    whoops... neglected to replace credentials with getenv() changed the postgres user's password as well

--- scripts/test-dbt-setup.py
+++ scripts/test-dbt-setup.py
@@ -1,8 +1,9 @@
-import requests
 import argparse
-import sys
-from faker import Faker
 from testclient import ClientTester
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', default=8000)
@@ -11,20 +12,26 @@ parser.add_argument('--password', required=True)
 args = parser.parse_args()
 
 tester = ClientTester(args.port)
-
 tester.login(args.email, args.password)
+
+DBT_TARGETCONFIGS_TYPE = os.getenv('TESTING_DBT_TARGETCONFIGS_TYPE')
+DBT_TARGETCONFIGS_SCHEMA = os.getenv('TESTING_DBT_TARGETCONFIGS_SCHEMA')
+DBT_CREDENTIALS_USERNAME = os.getenv('TESTING_DBT_CREDENTIALS_USERNAME')
+DBT_CREDENTIALS_PASSWORD = os.getenv('TESTING_DBT_CREDENTIALS_PASSWORD')
+DBT_CREDENTIALS_DATABASE = os.getenv('TESTING_DBT_CREDENTIALS_DATABASE')
+DBT_CREDENTIALS_HOST = os.getenv('TESTING_DBT_CREDENTIALS_HOST')
+
 r = tester.clientpost('dbt/create/', json={
   'gitrepo_url': "https://github.com/DevDataPlatform/dbt_basictest.git",
-  'project_dir': "/Users/fatchat/openbrackets/ddp/dbt",
   'dbtversion': "1.4.5",
   'targetname': "dev",
-  'targettype': "postgres",
-  'targetschema': "public",
-  'host': "dev-test.c4hvhyuxrcet.ap-south-1.rds.amazonaws.com",
+  'targettype': DBT_TARGETCONFIGS_TYPE,
+  'targetschema': DBT_TARGETCONFIGS_SCHEMA,
+  'host': DBT_CREDENTIALS_HOST,
   'port': "5432",
-  'username': "usr_sim_warehouse",
-  'password': "j7gqxhrRPBTthSI",
-  'database': "sim_warehouse",
+  'username': DBT_CREDENTIALS_USERNAME,
+  'password': DBT_CREDENTIALS_PASSWORD,
+  'database': DBT_CREDENTIALS_DATABASE,
 })
 
 print(r)
