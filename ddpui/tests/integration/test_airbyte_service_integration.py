93a2be7b3505bfa613223ebfcc1f9f37220fffbc
Abhishek-N <abhisheknshetty97@gmail.com>
2023-04-11 16:19:30 +0530

    update requirements and setup testing framework

--- ddpui/tests/integration/test_airbyte_service_integration.py
+++ ddpui/tests/integration/test_airbyte_service_integration.py
@@ -1,2 +1,21 @@
-import django
 import os
+from unittest.mock import patch
+import django
+import pytest
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+
+from ddpui.ddpairbyte.airbyte_service import *
+
+
+class TestWorkspaceIntegration:
+    
+    def test_get_workspaces(self):
+        result = get_workspaces()
+        assert isinstance(result, dict)
+        assert 'workspaces' in result
+        workspaces = result['workspaces']
+        assert isinstance(workspaces, list)
+        assert all(isinstance(workspace, dict) for workspace in workspaces)
