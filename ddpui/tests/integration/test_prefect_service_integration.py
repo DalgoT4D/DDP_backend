58fc9b0e0d740148cb99a0595ba7da940924e714
Abhishek-N <abhisheknshetty97@gmail.com>
2023-04-14 17:32:05 +0530

    remove unneeded files

--- ddpui/tests/integration/test_prefect_service_integration.py
+++
@@ -1,8 +0,0 @@
-import django
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
-os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-django.setup()
-
-from ddpui.ddpairbyte.airbyte_service import *
