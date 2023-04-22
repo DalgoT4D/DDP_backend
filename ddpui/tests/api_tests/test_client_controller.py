b28940a38a218449ab679ff1c367cfffab4d554c
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-04-23 00:36:11 +0530

    commented out deprecated import

--- ddpui/tests/api_tests/test_client_controller.py
+++ ddpui/tests/api_tests/test_client_controller.py
@@ -7,5 +7,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 
-from ddpui.api.client_controller import *
+# from ddpui.api.client_controller import *
 
