5b0b0f36ee2b80f4450a0ae6e6645ad567ae9de8
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-04-27 09:37:58 +0530

    celery for long running tasks

--- ddpui/urls.py
+++ ddpui/urls.py
@@ -6,6 +6,7 @@ from ddpui.api.client.airbyte_api import airbyteapi
 from ddpui.api.client.dbt_api import dbtapi
 from ddpui.api.client.prefect_api import prefectapi
 from ddpui.api.client.user_org_api import user_org_api
+from ddpui.api.client.task_api import taskapi
 
 urlpatterns = [
     # path("admin/", admin.site.urls), # Uncomment if you want to use django-admin app
@@ -13,5 +14,6 @@ urlpatterns = [
     path("api/airbyte/", airbyteapi.urls),
     path("api/dbt/", dbtapi.urls),
     path("api/prefect/", prefectapi.urls),
+    path("api/tasks/", taskapi.urls),
     path("api/", user_org_api.urls),
 ]
---
+++ ddpui/utils/taskprogress.py
@@ -0,0 +1,25 @@
+"""simple helper for a celery task to update its progress for its invoker to check on"""
+import json
+from redis import Redis
+
+class TaskProgress:
+    """maintain a list of steps, store in redis under the name "taskprogress" 
+    with the task_id as the key"""
+    def __init__(self, task_id) -> None:
+        self.task_id = task_id
+        self.taskprogress = []
+        self.redis = Redis()
+
+    def add(self, progress) -> None:
+        """append the latest progress to the list and update in redis"""
+        self.taskprogress.append(progress)
+        self.redis.hset('taskprogress', self.task_id, json.dumps(self.taskprogress))
+
+    @staticmethod
+    def fetch(task_id):
+        """look up progress by task_id"""
+        redis = Redis()
+        result = redis.hget('taskprogress', task_id)
+        if result:
+            return json.loads(result)
+        return None
