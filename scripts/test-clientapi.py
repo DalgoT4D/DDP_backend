cb9d0399d595f65b57e5841fe2c86b9b46d6c1e2
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-05-08 18:45:16 +0530

    new payload for /organizations/warehouse/

--- scripts/test-clientapi.py
+++ scripts/test-clientapi.py
@@ -21,37 +21,68 @@ DBT_CREDENTIALS_HOST = os.getenv("TESTING_DBT_CREDENTIALS_HOST")
 DBT_BIGQUERY_SERVICE_ACCOUNT_CREDSFILE = os.getenv(
     "TESTING_DBT_BIGQUERY_SERVICE_ACCOUNT_CREDSFILE"
 )
+BIGQUERY_PROJECTID = os.getenv("TESTING_BIGQUERY_PROJECTID")
+BIGQUERY_DATASETID = os.getenv("TESTING_BIGQUERY_DATASETID")
+BIGQUERY_DATASETLOCATION = os.getenv("TESTING_BIGQUERY_DATASETLOCATION")
+
 DBT_TEST_REPO = os.getenv("TESTING_DBT_TEST_REPO")
 DBT_TEST_REPO_ACCESSTOKEN = os.getenv("TESTING_DBT_TEST_REPO_ACCESSTOKEN")
 
-faker = Faker('en-IN')
-tester = TestClient(8002)
+faker = Faker("en-IN")
+tester = TestClient(8002, verbose=False)
 email = faker.email()
 password = faker.password()
-tester.clientpost("organizations/users/", json={
-    'email': email,
-    'password': password
-})
+tester.clientpost("organizations/users/", json={"email": email, "password": password})
 tester.login(email, password)
 tester.clientget("currentuser")
-tester.clientpost('organizations/', json={
-    "name": faker.company()[:20],
-})
+tester.clientpost(
+    "organizations/",
+    json={
+        "name": faker.company()[:20],
+    },
+)
 # tester.clientdelete("organizations/warehouses/")
 
-credentials = None
+
+# dbtCredentials = None
+# airbyteConfig = None
+# destinationDefinitionId = None
+
+destination_definitions = tester.clientget("airbyte/destination_definitions")
 if WAREHOUSETYPE == "postgres":
-    credentials = {
+    for destdef in destination_definitions:
+        if destdef["name"] == "Postgres":
+            destinationDefinitionId = destdef["destinationDefinitionId"]
+            break
+    dbtCredentials = {
         "host": DBT_CREDENTIALS_HOST,
-        "port": "5432",
+        "port": 5432,
         "username": DBT_CREDENTIALS_USERNAME,
         "password": DBT_CREDENTIALS_PASSWORD,
         "database": DBT_CREDENTIALS_DATABASE,
     }
+    airbyteConfig = {
+        "host": DBT_CREDENTIALS_HOST,
+        "port": 5432,
+        "username": DBT_CREDENTIALS_USERNAME,
+        "password": DBT_CREDENTIALS_PASSWORD,
+        "database": DBT_CREDENTIALS_DATABASE,
+        "schema": DBT_TARGETCONFIGS_SCHEMA,
+    }
 elif WAREHOUSETYPE == "bigquery":
+    for destdef in destination_definitions:
+        if destdef["name"] == "BigQuery":
+            destinationDefinitionId = destdef["destinationDefinitionId"]
+            break
     with open(DBT_BIGQUERY_SERVICE_ACCOUNT_CREDSFILE, "r", -1, "utf8") as credsfile:
-        credentials = json.loads(credsfile.read())
-        print(credentials)
+        dbtCredentials = json.loads(credsfile.read())
+        # print(dbtCredentials)
+    airbyteConfig = {
+        "project_id": BIGQUERY_PROJECTID,
+        "dataset_id": BIGQUERY_DATASETID,
+        "dataset_location": BIGQUERY_DATASETLOCATION,
+        "credentials_json": json.dumps(dbtCredentials),
+    }
 else:
     raise Exception("unknown WAREHOUSETYPE " + WAREHOUSETYPE)
 
@@ -59,7 +90,9 @@ tester.clientpost(
     "organizations/warehouse/",
     json={
         "wtype": WAREHOUSETYPE,
-        "credentials": credentials,
+        "destinationDefId": destinationDefinitionId,
+        "airbyteConfig": airbyteConfig,
+        "dbtCredentials": dbtCredentials,
     },
 )
 
@@ -79,22 +112,22 @@ if True:
         },
         timeout=30,
     )
-    task_id = r['task_id']
+    task_id = r["task_id"]
     while True:
         sleep(2)
-        resp = tester.clientget('tasks/' + task_id)
-        if 'progress' not in resp:
+        resp = tester.clientget("tasks/" + task_id)
+        if "progress" not in resp:
             continue
-        progress = resp['progress']
+        progress = resp["progress"]
         laststatus = None
         for step in progress:
             print(step)
-            laststatus = step['status']
-        if laststatus in ['failed', 'completed']:
+            laststatus = step["status"]
+        if laststatus in ["failed", "completed"]:
             break
         print("=" * 40)
 
-if True:
+if False:
     tester.clientpost("dbt/git_pull/")
 
 tester.clientdelete("prefect/blocks/dbt/")
@@ -113,7 +146,7 @@ r = tester.clientpost(
 print(r)
 
 tester.clientpost(
-    "prefect/flows/dbt_run/", json={"blockName": r['block_name']}, timeout=60
+    "prefect/flows/dbt_run/", json={"blockName": r["block_name"]}, timeout=60
 )
 
 # tester.clientdelete("prefect/blocks/dbt/")
