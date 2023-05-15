4b615e3e171ac56b69f05257f51278252f291516
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-05-15 14:25:22 +0530

    put the dbt profile name and the target schema name into the dbt block names for uniqueness and tracking

--- ddpui/api/client/prefect_api.py
+++ ddpui/api/client/prefect_api.py
@@ -178,7 +178,12 @@ def post_prefect_dbt_core_run_flow(
 
 @prefectapi.post("/blocks/dbt/", auth=auth.CanManagePipelines())
 def post_prefect_dbt_core_block(request, payload: PrefectDbtRun):
-    """Create prefect dbt core block"""
+    """Create three prefect dbt core blocks:
+    - dbt run
+    - dbt test
+    - dbt docs generate
+    for a ddp-dbt-profile
+    """
     orguser = request.orguser
     if orguser.org.dbt is None:
         raise HttpError(400, "create a dbt workspace first")
@@ -195,10 +200,16 @@ def post_prefect_dbt_core_block(request, payload: PrefectDbtRun):
     dbt_binary = str(dbt_env_dir / "venv/bin/dbt")
     project_dir = str(dbt_env_dir / "dbtrepo")
 
+    schema = (
+        payload.profile.target_configs_schema
+        if payload.profile.target_configs_schema
+        else orguser.org.dbt.default_schema
+    )
+
     block_names = []
     sequence_number = 0
     for command in ["docs generate", "run", "test"]:
-        block_name = f"{orguser.org.slug}-{slugify(command)}"
+        block_name = f"{orguser.org.slug}-{slugify(payload.profile.name)}-{slugify(schema)}-{slugify(command)}"
 
         block_data = PrefectDbtCoreSetup(
             block_name=block_name,
