4c3e8d54680295f04776c39b58ddb6ffd2f91a80
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-06-05 17:31:41 +0530

    remove unused

--- ddpui/utils/helpers.py
+++ ddpui/utils/helpers.py
@@ -1,42 +1,10 @@
 import shlex
 import subprocess
-from typing import Union
-from typing import List
-import humps
 
 
 def runcmd(cmd, cwd):
     """runs a shell command in a specified working directory"""
     return subprocess.run(shlex.split(cmd), cwd=str(cwd), check=True)
-
-
-def api_res_camel_case(api_res: Union[List[dict], dict, str]):
-    """Convert strings and dictionary keys to camel case"""
-    if isinstance(api_res, str):
-        return humps.camelize(api_res)
-
-    if isinstance(api_res, dict):
-        camel_dict_obj = {}
-        for old_key, value in api_res.items():
-            new_key = humps.camelize(old_key)
-            camel_dict_obj[new_key] = value
-
-        return camel_dict_obj
-
-    if isinstance(api_res, list):
-        ans = []
-        for dict_obj in api_res:
-            camel_dict_obj = {}
-            if isinstance(dict_obj, dict):
-                for old_key, value in dict_obj.items():
-                    new_key = humps.camelize(old_key)
-                    camel_dict_obj[new_key] = value
-
-            ans.append(camel_dict_obj)
-
-        return ans
-
-    return api_res
 
 
 def remove_nested_attribute(obj: dict, attr: str) -> dict:
