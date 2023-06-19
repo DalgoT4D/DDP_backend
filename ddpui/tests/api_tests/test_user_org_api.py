210366f8df77495d8bccd9d839ae9ce396293237
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-06-19 16:23:40 +0530

    finished tests

--- ddpui/tests/api_tests/test_user_org_api.py
+++ ddpui/tests/api_tests/test_user_org_api.py
@@ -1,5 +1,6 @@
 import os
 import django
+from datetime import datetime
 
 from unittest.mock import Mock, patch
 import pytest
@@ -24,9 +25,18 @@ from ddpui.api.client.user_org_api import (
     post_organization_user_accept_invite,
 )
 from ddpui.models.org import Org, OrgSchema, OrgWarehouseSchema, OrgWarehouse
-from ddpui.models.org_user import OrgUser, OrgUserCreate, OrgUserRole, OrgUserUpdate
+from ddpui.models.org_user import (
+    OrgUser,
+    OrgUserCreate,
+    OrgUserRole,
+    OrgUserUpdate,
+    InvitationSchema,
+    Invitation,
+    AcceptInvitationSchema,
+)
 from django.contrib.auth.models import User
 from ddpui.ddpairbyte.schema import AirbyteWorkspace
+from ddpui.utils import timezone
 
 
 # ================================================================================
@@ -378,3 +388,233 @@ def test_delete_organization_warehouses(orguser):
 
 
 # ================================================================================
+@patch.multiple(
+    "ddpui.ddpairbyte.airbyte_service",
+    get_destination=Mock(
+        side_effect=[
+            {
+                "destination_id": "destination_id_1",
+            },
+            {
+                "destination_id": "destination_id_2",
+            },
+        ]
+    ),
+)
+def test_get_organizations_warehouses(orguser):
+    mock_request = Mock()
+    mock_request.orguser = orguser
+
+    while True:
+        warehouse = OrgWarehouse.objects.filter(org=orguser.org).first()
+        if warehouse:
+            warehouse.delete()
+        else:
+            break
+
+    warehouse1 = OrgWarehouse.objects.create(
+        org=orguser.org,
+        wtype="postgres",
+        airbyte_destination_id="destination_id_1",
+    )
+    warehouse2 = OrgWarehouse.objects.create(
+        org=orguser.org,
+        wtype="postgres",
+        airbyte_destination_id="destination_id_2",
+    )
+    response = get_organizations_warehouses(mock_request)
+    assert "warehouses" in response
+    assert len(response["warehouses"]) == 2
+    assert response["warehouses"][0]["wtype"] == "postgres"
+    assert (
+        response["warehouses"][0]["airbyte_destination"]["destination_id"]
+        == "destination_id_1"
+    )
+    assert response["warehouses"][1]["wtype"] == "postgres"
+    assert (
+        response["warehouses"][1]["airbyte_destination"]["destination_id"]
+        == "destination_id_2"
+    )
+    warehouse1.delete()
+    warehouse2.delete()
+
+
+# ================================================================================
+def test_post_organization_user_invite_failure(orguser):
+    payload = InvitationSchema(
+        invited_email="inivted_email",
+        invited_role=1,
+        invited_by=None,
+        invited_on=timezone.as_ist(datetime.now()),
+        invite_code="invite_code",
+    )
+    invitation = Invitation.objects.create(
+        invited_email=payload.invited_email,
+        invited_by=orguser,
+        invited_on=timezone.as_ist(datetime.now()),
+    )
+    mock_request = Mock()
+    mock_request.orguser = orguser
+    with pytest.raises(HttpError) as excinfo:
+        post_organization_user_invite(mock_request, payload)
+    assert str(excinfo.value) == f"{payload.invited_email} has already been invited"
+    invitation.delete()
+
+
+def test_post_organization_user_invite(orguser):
+    payload = InvitationSchema(
+        invited_email="inivted_email",
+        invited_role=1,
+        invited_by=None,
+        invited_on=timezone.as_ist(datetime.now()),
+        invite_code="invite_code",
+    )
+
+    while True:
+        invitation = Invitation.objects.filter(
+            invited_email=payload.invited_email, invited_by=orguser
+        ).first()
+        if invitation:
+            invitation.delete()
+        else:
+            break
+
+    mock_request = Mock()
+    mock_request.orguser = orguser
+
+    assert (
+        Invitation.objects.filter(
+            invited_email=payload.invited_email, invited_by=orguser
+        ).count()
+        == 0
+    )
+    response = post_organization_user_invite(mock_request, payload)
+    assert (
+        Invitation.objects.filter(
+            invited_email=payload.invited_email, invited_by=orguser
+        ).count()
+        == 1
+    )
+
+    assert response.invited_by.email == orguser.user.email
+    assert response.invited_by.role == orguser.role
+    assert response.invited_role == payload.invited_role
+    assert response.invited_on == payload.invited_on
+    assert response.invite_code == payload.invite_code
+
+
+# ================================================================================
+def test_get_organization_user_invite_fail(orguser):
+    invited_email = "invited_email"
+    while True:
+        invitation = Invitation.objects.filter(
+            invited_email=invited_email, invited_by=orguser
+        ).first()
+        if invitation:
+            invitation.delete()
+        else:
+            break
+
+    invitation = Invitation.objects.create(
+        invited_email=invited_email,
+        invited_by=orguser,
+        invited_on=timezone.as_ist(datetime.now()),
+        invite_code="invite_code",
+    )
+    mock_request = Mock()
+    mock_request.orguser = orguser
+
+    with pytest.raises(HttpError) as excinfo:
+        get_organization_user_invite(mock_request, "wrong-code")
+    assert str(excinfo.value) == "invalid invite code"
+
+    invitation.delete()
+
+
+def test_get_organization_user_invite(orguser):
+    invited_email = "invited_email"
+    while True:
+        invitation = Invitation.objects.filter(
+            invited_email=invited_email, invited_by=orguser
+        ).first()
+        if invitation:
+            invitation.delete()
+        else:
+            break
+
+    invitation = Invitation.objects.create(
+        invited_email=invited_email,
+        invited_by=orguser,
+        invited_on=timezone.as_ist(datetime.now()),
+        invite_code="invite_code",
+    )
+    mock_request = Mock()
+    mock_request.orguser = orguser
+
+    response = get_organization_user_invite(mock_request, "invite_code")
+    assert response.invited_email == invitation.invited_email
+    assert response.invited_role == invitation.invited_role
+    assert response.invited_by.email == invitation.invited_by.user.email
+    assert response.invited_on == invitation.invited_on
+    assert response.invite_code == invitation.invite_code
+
+    invitation.delete()
+
+
+# ================================================================================
+def test_post_organization_user_accept_invite_fail(orguser):
+    mock_request = Mock()
+    mock_request.orguser = orguser
+    payload = AcceptInvitationSchema(
+        invite_code="invalid-invite_code", password="password"
+    )
+
+    with pytest.raises(HttpError) as excinfo:
+        post_organization_user_accept_invite(mock_request, payload)
+
+    assert str(excinfo.value) == "invalid invite code"
+
+
+def test_post_organization_user_accept_invite(orguser):
+    mock_request = Mock()
+    mock_request.orguser = orguser
+    payload = AcceptInvitationSchema(invite_code="invite_code", password="password")
+    while True:
+        invitation = Invitation.objects.filter(
+            invited_email="invited_email", invited_by=orguser
+        ).first()
+        if invitation:
+            invitation.delete()
+        else:
+            break
+
+    invitation = Invitation.objects.create(
+        invited_email="invited_email",
+        invited_by=orguser,
+        invited_on=timezone.as_ist(datetime.now()),
+        invite_code="invite_code",
+    )
+
+    while True:
+        orguser = OrgUser.objects.filter(
+            user__email="invited_email",
+        ).first()
+        if orguser:
+            orguser.delete()
+        else:
+            break
+
+    assert (
+        OrgUser.objects.filter(
+            user__email="invited_email",
+        ).count()
+        == 0
+    )
+    response = post_organization_user_accept_invite(mock_request, payload)
+    assert response.email == "invited_email"
+    assert (
+        OrgUser.objects.filter(
+            user__email="invited_email",
+        ).count()
+        == 1
+    )
