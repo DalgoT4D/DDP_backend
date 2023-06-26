30e10e6baa03295c32f5b9f66e979cadd1bbedf0
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-06-26 15:06:29 +0530

    send text message

--- ddpui/utils/sendgrid.py
+++ ddpui/utils/sendgrid.py
@@ -1,10 +1,31 @@
 import os
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, Content
 from ddpui.utils.ddp_logger import logger
 
 SENDGRID_APIKEY = os.getenv("SENDGRID_APIKEY")
 SENDGRID_SENDER = os.getenv("SENDGRID_SENDER")
+
+
+def send_text_message(to_email, subject, message):
+    """
+    send a plain-text email using sendgrid
+    """
+    sendgrid_client = SendGridAPIClient(SENDGRID_APIKEY)
+
+    content = Content("text/plain", message)
+    message = Mail(
+        SENDGRID_SENDER,
+        to_email,
+        subject,
+        content,
+    )
+
+    try:
+        sendgrid_client.send(message)
+    except Exception as error:
+        logger.exception(error)
+        raise
 
 
 def send_template_message(template_id: str, to_email: str, template_vars: dict) -> None:
@@ -14,7 +35,7 @@ def send_template_message(template_id: str, to_email: str, template_vars: dict) 
     """
     sendgrid_client = SendGridAPIClient(SENDGRID_APIKEY)
 
-    message = Mail(from_email=SENDGRID_SENDER, to_emails=[to_email])
+    message = Mail(from_email=SENDGRID_SENDER, to_email=to_email)
     message.template_id = template_id
     message.dynamic_template_data = template_vars
 
