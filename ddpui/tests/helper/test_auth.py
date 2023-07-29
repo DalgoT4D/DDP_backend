4f591c0fba5155f4a19d107ccc2e7bc8104df1f6
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-07-29 15:29:40 +0530

    support multiple OrgUsers per User

--- ddpui/tests/helper/test_auth.py
+++ ddpui/tests/helper/test_auth.py
@@ -50,16 +50,18 @@ def admin_user(user: User):
 
 @pytest.fixture
 def org():
-    temp_org = Org.objects.create(name="temp-org")
+    temp_org = Org.objects.create(name="temp-org", slug="temp-org")
     yield temp_org
     temp_org.delete()
 
 
+def create_accountmanager(user: User, org: Org):
+    return OrgUser.objects.create(user=user, org=org, role=OrgUserRole.ACCOUNT_MANAGER)
+
+
 @pytest.fixture
 def org_user_accountmanager(user: User, org: Org):
-    temp_org_user = OrgUser.objects.create(
-        user=user, org=org, role=OrgUserRole.ACCOUNT_MANAGER
-    )
+    temp_org_user = create_accountmanager(user, org)
     yield temp_org_user
     temp_org_user.delete()
 
@@ -85,7 +87,7 @@ def org_user_reportviewer(user: User, org: Org):
 # ====================================================================================
 def test_platformadmin_authenticate_no_tokenrecord():
     obj = PlatformAdmin()
-    request = Mock()
+    request = Mock(headers={})
     with pytest.raises(HttpError) as excinfo:
         obj.authenticate(request, "key-dne")
     assert str(excinfo.value) == UNAUTHORIZED
@@ -93,7 +95,7 @@ def test_platformadmin_authenticate_no_tokenrecord():
 
 def test_platformadmin_authenticate_no_adminuser(user: User):
     obj = PlatformAdmin()
-    request = Mock()
+    request = Mock(headers={})
     test_token = token(user)
     with pytest.raises(HttpError) as excinfo:
         obj.authenticate(request, test_token.key)
@@ -102,7 +104,7 @@ def test_platformadmin_authenticate_no_adminuser(user: User):
 
 def test_platformadmin_authenticate(admin_user: AdminUser):
     obj = PlatformAdmin()
-    request = Mock()
+    request = Mock(headers={})
     test_token = token(admin_user.user)
     trecord = obj.authenticate(request, test_token.key)
     assert trecord == test_token
@@ -112,7 +114,7 @@ def test_platformadmin_authenticate(admin_user: AdminUser):
 # ====================================================================================
 def test_authenticate_org_user_require_org(org_user_accountmanager: OrgUser):
     test_token = token(org_user_accountmanager.user)
-    request = Mock()
+    request = Mock(headers={})
     allowed_roles = [org_user_accountmanager.role]
     org_user_accountmanager.org = None
     org_user_accountmanager.save()
@@ -126,7 +128,7 @@ def test_authenticate_org_user_require_org(org_user_accountmanager: OrgUser):
 def test_authenticate_org_user_unauthorized_no_tokenrecord(
     org_user_accountmanager: OrgUser,
 ):
-    request = Mock()
+    request = Mock(headers={})
     allowed_roles = [org_user_accountmanager.role]
     org_user_accountmanager.org = None
     org_user_accountmanager.save()
@@ -140,7 +142,7 @@ def test_authenticate_org_user_unauthorized_no_tokenrecord(
 def test_authenticate_org_user_unauthorized_no_orguser():
     another_user = create_user()
     test_token = token(another_user)
-    request = Mock()
+    request = Mock(headers={})
     allowed_roles = []
     require_org = True
 
@@ -153,7 +155,7 @@ def test_authenticate_org_user_unauthorized_wrong_role(
     org_user_accountmanager: OrgUser,
 ):
     test_token = token(org_user_accountmanager.user)
-    request = Mock()
+    request = Mock(headers={})
     allowed_roles = [OrgUserRole.REPORT_VIEWER]
     require_org = True
 
@@ -164,7 +166,7 @@ def test_authenticate_org_user_unauthorized_wrong_role(
 
 def test_authenticate_org_user_success(org_user_accountmanager: OrgUser):
     test_token = token(org_user_accountmanager.user)
-    request = Mock()
+    request = Mock(headers={})
     allowed_roles = [org_user_accountmanager.role]
     require_org = True
 
@@ -175,10 +177,48 @@ def test_authenticate_org_user_success(org_user_accountmanager: OrgUser):
     assert response.orguser == org_user_accountmanager
 
 
+@pytest.fixture
+def anotherorg():
+    temp_org = Org.objects.create(name="another-temp-org", slug="another-temp-org")
+    yield temp_org
+    temp_org.delete()
+
+
+def test_authenticate_org_user_select_org_success(
+    user: User, org: Org, anotherorg: Org
+):
+    orguser1 = create_accountmanager(user, org)
+    orguser2 = create_accountmanager(user, anotherorg)
+
+    test_token = token(user)
+    request = Mock(headers={})
+    allowed_roles = [OrgUserRole.ACCOUNT_MANAGER]
+    require_org = True
+
+    request.headers = {"x-kaapi-org": org.slug}
+    response = authenticate_org_user(
+        request, test_token.key, allowed_roles, require_org
+    )
+    assert response == request
+    assert response.orguser == orguser1
+
+    request.headers = {"x-kaapi-org": anotherorg.slug}
+    response = authenticate_org_user(
+        request, test_token.key, allowed_roles, require_org
+    )
+    assert response == request
+    assert response.orguser == orguser2
+
+    request.headers = {"x-kaapi-org": "dne-slug"}
+    with pytest.raises(HttpError) as excinfo:
+        authenticate_org_user(request, test_token.key, allowed_roles, require_org)
+    assert str(excinfo.value) == UNAUTHORIZED
+
+
 # ====================================================================================
 def test_anyorguser_accountmanager_success(org_user_accountmanager: OrgUser):
     aou = AnyOrgUser()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_accountmanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_accountmanager
@@ -187,7 +227,7 @@ def test_anyorguser_accountmanager_success(org_user_accountmanager: OrgUser):
 
 def test_anyorguser_pipelinemanager_success(org_user_pipelinemanager: OrgUser):
     aou = AnyOrgUser()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_pipelinemanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_pipelinemanager
@@ -196,7 +236,7 @@ def test_anyorguser_pipelinemanager_success(org_user_pipelinemanager: OrgUser):
 
 def test_anyorguser_reportviewer_success(org_user_reportviewer: OrgUser):
     aou = AnyOrgUser()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_reportviewer.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_reportviewer
@@ -206,7 +246,7 @@ def test_anyorguser_reportviewer_success(org_user_reportviewer: OrgUser):
 # ====================================================================================
 def test_canmanagepipelines_accountmanager_success(org_user_accountmanager: OrgUser):
     aou = CanManagePipelines()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_accountmanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_accountmanager
@@ -215,7 +255,7 @@ def test_canmanagepipelines_accountmanager_success(org_user_accountmanager: OrgU
 
 def test_canmanagepipelines_pipelinemanager_success(org_user_pipelinemanager: OrgUser):
     aou = CanManagePipelines()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_pipelinemanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_pipelinemanager
@@ -224,7 +264,7 @@ def test_canmanagepipelines_pipelinemanager_success(org_user_pipelinemanager: Or
 
 def test_canmanagepipelines_reportviewer_success(org_user_reportviewer: OrgUser):
     aou = CanManagePipelines()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_reportviewer.user)
     with pytest.raises(HttpError) as excinfo:
         aou.authenticate(request, temp_token)
@@ -234,7 +274,7 @@ def test_canmanagepipelines_reportviewer_success(org_user_reportviewer: OrgUser)
 # ====================================================================================
 def test_canmanageusers_accountmanager_success(org_user_accountmanager: OrgUser):
     aou = CanManageUsers()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_accountmanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_accountmanager
@@ -243,7 +283,7 @@ def test_canmanageusers_accountmanager_success(org_user_accountmanager: OrgUser)
 
 def test_canmanageusers_pipelinemanager_success(org_user_pipelinemanager: OrgUser):
     aou = CanManageUsers()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_pipelinemanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_pipelinemanager
@@ -252,7 +292,7 @@ def test_canmanageusers_pipelinemanager_success(org_user_pipelinemanager: OrgUse
 
 def test_canmanageusers_reportviewer_success(org_user_reportviewer: OrgUser):
     aou = CanManageUsers()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_reportviewer.user)
     with pytest.raises(HttpError) as excinfo:
         aou.authenticate(request, temp_token)
@@ -262,7 +302,7 @@ def test_canmanageusers_reportviewer_success(org_user_reportviewer: OrgUser):
 # ====================================================================================
 def test_fullaccess_accountmanager_success(org_user_accountmanager: OrgUser):
     aou = FullAccess()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_accountmanager.user)
     response = aou.authenticate(request, temp_token)
     assert response.orguser == org_user_accountmanager
@@ -271,7 +311,7 @@ def test_fullaccess_accountmanager_success(org_user_accountmanager: OrgUser):
 
 def test_fullaccess_pipelinemanager_success(org_user_pipelinemanager: OrgUser):
     aou = FullAccess()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_pipelinemanager.user)
     with pytest.raises(HttpError) as excinfo:
         aou.authenticate(request, temp_token)
@@ -280,7 +320,7 @@ def test_fullaccess_pipelinemanager_success(org_user_pipelinemanager: OrgUser):
 
 def test_fullaccess_reportviewer_success(org_user_reportviewer: OrgUser):
     aou = FullAccess()
-    request = Mock
+    request = Mock(headers={})
     temp_token = Token.objects.create(key="ttt", user=org_user_reportviewer.user)
     with pytest.raises(HttpError) as excinfo:
         aou.authenticate(request, temp_token)
