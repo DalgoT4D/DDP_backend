ab8a470671d1c957927016de5c761c8f2d85f8fc
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-07-30 15:46:27 +0530

    suppgress deepsource warnings (temporarily... will resolve this week)

--- ddpui/models/org.py
+++ ddpui/models/org.py
@@ -6,7 +6,9 @@ class OrgDbt(models.Model):
     """Docstring"""
 
     gitrepo_url = models.CharField(max_length=100)
-    gitrepo_access_token_secret = models.CharField(max_length=100, null=True)
+    gitrepo_access_token_secret = models.CharField(
+        max_length=100, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
 
     project_dir = models.CharField(max_length=200)
     dbt_version = models.CharField(max_length=10)
@@ -22,9 +24,15 @@ class Org(models.Model):
     """Docstring"""
 
     name = models.CharField(max_length=50)
-    slug = models.CharField(max_length=20, null=True)
-    airbyte_workspace_id = models.CharField(max_length=36, null=True)
-    dbt = models.ForeignKey(OrgDbt, on_delete=models.SET_NULL, null=True)
+    slug = models.CharField(
+        max_length=20, null=True
+    )  # skipcq: PTC-W0901 skipcq: PTC-W0906
+    airbyte_workspace_id = models.CharField(
+        max_length=36, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    dbt = models.ForeignKey(
+        OrgDbt, on_delete=models.SET_NULL, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
 
     def __str__(self) -> str:
         return f"Org[{self.slug}|{self.name}|{self.airbyte_workspace_id}]"
@@ -39,10 +47,16 @@ class OrgPrefectBlock(models.Model):
     block_name = models.CharField(
         max_length=100, unique=True
     )  # use blockname to distinguish between different dbt commands
-    display_name = models.CharField(max_length=100, null=True)
-    command = models.CharField(max_length=100, null=True)
-    dbt_target_schema = models.CharField(max_length=50, null=True)
-    seq = models.SmallIntegerField(null=True)
+    display_name = models.CharField(
+        max_length=100, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    command = models.CharField(
+        max_length=100, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    dbt_target_schema = models.CharField(
+        max_length=50, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    seq = models.SmallIntegerField(null=True)  # skipcq: PTC-W0901, PTC-W0906
 
     def __str__(self) -> str:
         return f"OrgPrefectBlock[{self.org.name}|{self.block_type}|{self.block_name}]"
@@ -53,11 +67,17 @@ class OrgDataFlow(models.Model):
 
     org = models.ForeignKey(Org, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    deployment_name = models.CharField(max_length=100, null=True)
-    deployment_id = models.CharField(max_length=36, unique=True, null=True)
-    cron = models.CharField(max_length=36, null=True)
+    deployment_name = models.CharField(
+        max_length=100, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    deployment_id = models.CharField(
+        max_length=36, unique=True, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    cron = models.CharField(max_length=36, null=True)  # skipcq: PTC-W0901, PTC-W0906
     # and if deployment is manual airbyte-connection-sync,then we store the conn_id
-    connection_id = models.CharField(max_length=36, unique=True, null=True)
+    connection_id = models.CharField(
+        max_length=36, unique=True, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
 
     def __str__(self) -> str:
         return f"OrgDataFlow[{self.name}|{self.deployment_name}|{self.deployment_id}|{self.cron}|{self.connection_id}]"
@@ -77,8 +97,12 @@ class OrgWarehouse(models.Model):
     wtype = models.CharField(max_length=25)  # postgres, bigquery
     credentials = models.CharField(max_length=200)
     org = models.ForeignKey(Org, on_delete=models.CASCADE)
-    airbyte_destination_id = models.TextField(max_length=36, null=True)
-    airbyte_norm_op_id = models.TextField(max_length=36, null=True)
+    airbyte_destination_id = models.TextField(
+        max_length=36, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
+    airbyte_norm_op_id = models.TextField(
+        max_length=36, null=True
+    )  # skipcq: PTC-W0901, PTC-W0906
 
     def __str__(self) -> str:
         return (
