0116d19c63720da18f0314d01a7f67a805da352e
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-09-08 16:09:07 +0530

    refactoring for future use

--- ddpui/api/client/webhook_api.py
+++ ddpui/api/client/webhook_api.py
@@ -7,6 +7,7 @@ from ninja.errors import HttpError
 from ddpui.utils.custom_logger import CustomLogger
 from ddpui.api.client.prefect_api import prefect_service
 from ddpui.models.org import OrgPrefectBlock, Org
+from ddpui.models.orgjobs import BlockLock
 from ddpui.models.org_user import OrgUser, OrgUserRole
 from ddpui.utils.sendgrid import send_text_message
 
@@ -16,62 +17,40 @@ webhookapi = NinjaAPI(urls_namespace="webhook")
 
 logger = CustomLogger("ddpui")
 
+FLOW_RUN = "flow-run"
+FLOW = "flow"
+DEPLOYMENT = "deployment"
 
-@webhookapi.post("/notification/")
-def post_notification(request):  # pylint: disable=unused-argument
-    """webhook endpoint for notifications"""
-    if request.headers.get("X-Notification-Key") != os.getenv(
-        "PREFECT_NOTIFICATIONS_WEBHOOK_KEY"
-    ):
-        raise HttpError(400, "unauthorized")
-    notification = json.loads(request.body)
-    message = notification["body"]
-    logger.info(message)
-    # <empty line>
-    # Flow run return-dbt-test-failed/lush-elephant entered state `DBT_TEST_FAILED` at 2023-08-26T08:09:20.159919+00:00.
-    # <empty line>
-    # Flow ID: c7266896-10b0-4c65-a503-dce17d4de807
-    # Flow run ID: acad1e73-4d23-4d38-9a1a-4ce36631971f
-    # Flow run URL: http://127.0.0.1:4200/flow-runs/flow-run/acad1e73-4d23-4d38-9a1a-4ce36631971f
-    # State message: WARNING: test failed
-    notification_types = [
-        {"pattern": "Flow run ID: (.*)", "name": "flow-run-id"},
-        {"pattern": "State message: (.*)", "name": "state"},
-    ]
-    for notification_type in notification_types:
-        match = re.search(notification_type["pattern"], message)
 
-        if match:
-            match_data = match.groups()[0]
-            logger.info("found %s %s", notification_type["name"], match_data)
+def get_message_type(message_object: dict) -> str | None:
+    """identifies the message type if possible"""
+    if message_object.get("state"):
+        if message_object["state"].get("state_details"):
+            if (
+                message_object["state"]["state_details"].get("flow_run_id")
+                == message_object["id"]
+            ):
+                return FLOW_RUN
 
-            if notification_type["name"] == "flow-run-id":
-                flow_run_id = match_data
-                flow_run = prefect_service.get_flow_run(flow_run_id)
-                logger.info(flow_run)
-                org = get_org_from_flow_run(flow_run)
+    return None
 
-                if org:
-                    logs = prefect_service.get_flow_run_logs(flow_run_id, 0)
-                    logmessages = [x["message"] for x in logs["logs"]]
-                    email_body = generate_notification_email(
-                        org.name, flow_run_id, logmessages
-                    )
-                    for orguser in OrgUser.objects.filter(
-                        org=org,
-                        role__in=[
-                            OrgUserRole.ACCOUNT_MANAGER,
-                            OrgUserRole.PIPELINE_MANAGER,
-                        ],
-                    ):
-                        logger.info(
-                            f"sending prefect-notification email to {orguser.user.email}"
-                        )
-                        send_text_message(
-                            orguser.user.email, "Prefect notificatdion", email_body
-                        )
 
-    return {"status": "ok"}
+def get_flow_run_id_from_logs(message: str) -> str | None:
+    """uses regex matching to try to get a flow-run id"""
+    match = re.search("Flow run ID: (.*)", message)
+    if match:
+        flow_run_id = match.groups()[0]
+        return flow_run_id
+    return None
+
+
+def get_state_message_from_logs(message: str) -> str | None:
+    """uses regex matching to try to get a flow-run id"""
+    match = re.search("State message: (.*)", message)
+    if match:
+        state_message = match.groups()[0]
+        return state_message
+    return None
 
 
 def get_org_from_flow_run(flow_run: dict) -> Org | None:
@@ -125,3 +104,73 @@ Logs:
 """
     email_body += "\n".join(logmessages)
     return email_body
+
+
+def email_orgusers(org: Org, email_body: str):
+    """sends a notificationemail to all OrgUsers"""
+    for orguser in OrgUser.objects.filter(
+        org=org,
+        role__in=[
+            OrgUserRole.ACCOUNT_MANAGER,
+            OrgUserRole.PIPELINE_MANAGER,
+        ],
+    ):
+        logger.info(f"sending prefect-notification email to {orguser.user.email}")
+        send_text_message(orguser.user.email, "Prefect notificatdion", email_body)
+
+
+def email_flowrun_logs_to_orgusers(org: Org, flow_run_id: str):
+    """retrieves logs for a flow-run and emails them to all users for the org"""
+    logs = prefect_service.get_flow_run_logs(flow_run_id, 0)
+    logmessages = [x["message"] for x in logs["logs"]]
+    email_body = generate_notification_email(org.name, flow_run_id, logmessages)
+    email_orgusers(org, email_body)
+
+
+@webhookapi.post("/notification/")
+def post_notification(request):  # pylint: disable=unused-argument
+    """webhook endpoint for notifications"""
+    if request.headers.get("X-Notification-Key") != os.getenv(
+        "PREFECT_NOTIFICATIONS_WEBHOOK_KEY"
+    ):
+        raise HttpError(400, "unauthorized")
+    notification = json.loads(request.body)
+    message = notification["body"]
+    logger.info(message)
+
+    message_object = None
+    try:
+        message_object = json.loads(message)
+    except ValueError:
+        # not json, oh well
+        pass
+    if message_object is None and isinstance(message, dict):
+        message_object = message
+
+    flow_run_id = None
+    if message_object:
+        message_type = get_message_type(message_object)
+        if message_type == FLOW_RUN:
+            flow_run_id = message_object["id"]
+
+    else:
+        # <empty line>
+        # Flow run return-dbt-test-failed/lush-elephant entered state `DBT_TEST_FAILED` at 2023-08-26T08:09:20.159919+00:00.
+        # <empty line>
+        # Flow ID: c7266896-10b0-4c65-a503-dce17d4de807
+        # Flow run ID: acad1e73-4d23-4d38-9a1a-4ce36631971f
+        # Flow run URL: http://127.0.0.1:4200/flow-runs/flow-run/acad1e73-4d23-4d38-9a1a-4ce36631971f
+        # State message: WARNING: test failed
+
+        flow_run_id = get_flow_run_id_from_logs(message)
+
+    if flow_run_id:
+        logger.info("found flow-run id %s, retrieving flow-run", flow_run_id)
+        state = get_state_message_from_logs(message)
+        if state in ["WARNING: test failed"]:
+            BlockLock.objects.filter(flow_run_id=flow_run_id).delete()
+        # flow_run = prefect_service.get_flow_run(flow_run_id)
+        # logger.info(flow_run)
+        # org = get_org_from_flow_run(flow_run)
+
+    return {"status": "ok"}
