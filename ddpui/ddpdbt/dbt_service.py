fab75139284b25b9d0b8d4ee291143550b97dd03
Rohit Chatterjee <rohit@rootbridgeservices.in>
2023-09-15 16:44:14 +0530

    delete the git-pull block when deleting a dbt workspace

--- ddpui/ddpdbt/dbt_service.py
+++ ddpui/ddpdbt/dbt_service.py
@@ -3,7 +3,7 @@ import shutil
 from ddpui.models.org_user import Org
 from ddpui.models.org import OrgPrefectBlock
 from ddpui.ddpprefect import prefect_service
-from ddpui.ddpprefect import DBTCORE
+from ddpui.ddpprefect import DBTCORE, SHELLOPERATION
 from ddpui.utils import secretsmanager
 
 
@@ -20,5 +20,12 @@ def delete_dbt_workspace(org: Org):
     for dbtblock in OrgPrefectBlock.objects.filter(org=org, block_type=DBTCORE):
         prefect_service.delete_dbt_core_block(dbtblock.block_id)
         dbtblock.delete()
+
+    for shellblock in OrgPrefectBlock.objects.filter(
+        org=org, block_type=SHELLOPERATION
+    ):
+        if shellblock.block_name.find("-git-pull") > -1:
+            prefect_service.delete_shell_block(shellblock.block_id)
+            shellblock.delete()
 
     secretsmanager.delete_github_token(org)
