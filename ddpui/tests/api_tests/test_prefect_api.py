67b243c6126fbbe33004d6701345b67df80b9808
Ishan <koradiaishan335@gmail.com>
2023-12-12 08:19:07 +0530

    test case to fetch tasks

--- ddpui/tests/api_tests/test_prefect_api.py
+++ ddpui/tests/api_tests/test_prefect_api.py
@@ -7,12 +7,18 @@ from unittest.mock import Mock, patch, MagicMock
 import pytest
 from ninja.errors import HttpError
 
-from ddpui.api.client.prefect_api import post_prefect_transformation_tasks
-from ddpui.models.org import OrgDbt, Org, OrgWarehouse
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
+
+from ddpui.api.client.prefect_api import (
+    post_prefect_transformation_tasks,
+    get_prefect_transformation_tasks,
+)
+from ddpui.models.org import OrgDbt, Org, OrgWarehouse
+from ddpui.models.tasks import Task, OrgTask, OrgDataFlowv1, DataflowOrgTask
+from ddpui.utils.constants import TASK_DBTRUN
+
 
 pytestmark = pytest.mark.django_db
 
@@ -54,9 +60,60 @@ def org_with_dbt_workspace(tmpdir_factory):
     org.delete()
 
 
+@pytest.fixture()
+def org_with_transformation_tasks():
+    print("creating org with tasks")
+
+    org_slug = "test-org-slug"
+    org = Org.objects.create(
+        airbyte_workspace_id="FAKE-WORKSPACE-ID-1",
+        slug=org_slug,
+        name=org_slug,
+    )
+
+    # seed data
+    for task in [
+        {"type": "git", "slug": "git-pull", "label": "GIT pull", "command": "git pull"},
+        {"type": "dbt", "slug": "dbt-clean", "label": "DBT clean", "command": "clean"},
+        {"type": "dbt", "slug": "dbt-deps", "label": "DBT deps", "command": "deps"},
+        {"type": "dbt", "slug": "dbt-run", "label": "DBT run", "command": "run"},
+        {"type": "dbt", "slug": "dbt-test", "label": "DBT test", "command": "test"},
+        {
+            "type": "airbyte",
+            "slug": "airbyte-sync",
+            "label": "AIRBYTE sync",
+            "command": None,
+        },
+    ]:
+        Task.objects.create(**task)
+
+    for task in Task.objects.filter(type__in=["dbt", "git"]).all():
+        org_task = OrgTask.objects.create(org=org, task=task)
+
+        if task.slug == "dbt-run":
+            new_dataflow = OrgDataFlowv1.objects.create(
+                org=org,
+                name="test-dbtrun-deployment",
+                deployment_name="test-dbtrun-deployment",
+                deployment_id="test-dbtrun-deployment-id",
+                dataflow_type="manual",
+            )
+
+            DataflowOrgTask.objects.create(
+                dataflow=new_dataflow,
+                orgtask=org_task,
+            )
+
+        print("fxiture", org_task)
+
+    yield org
+
+    org.delete()
+
+
 # ================================================================================
 def test_post_prefect_transformation_tasks_dbt_not_setup():
-    """tests /tasks/transform/ without setting up dbt workspace"""
+    """tests POST /tasks/transform/ without setting up dbt workspace"""
     mock_orguser = Mock()
     mock_orguser.org.dbt = None
 
@@ -69,7 +126,7 @@ def test_post_prefect_transformation_tasks_dbt_not_setup():
 
 
 def test_post_prefect_transformation_tasks_warehouse_not_setup(org_with_dbt_workspace):
-    """tests /tasks/transform/ with no warehouse"""
+    """tests POST /tasks/transform/ with no warehouse"""
     mock_orguser = Mock()
     mock_orguser.org = org_with_dbt_workspace
 
@@ -111,7 +168,7 @@ def test_post_prefect_transformation_tasks_warehouse_not_setup(org_with_dbt_work
 def test_post_prefect_transformation_tasks_success_postgres_warehouse(
     org_with_dbt_workspace,
 ):
-    """tests /tasks/transform/ success with postgres warehouse"""
+    """tests POST /tasks/transform/ success with postgres warehouse"""
     mock_orguser = Mock()
     mock_orguser.org = org_with_dbt_workspace
 
@@ -159,7 +216,7 @@ def test_post_prefect_transformation_tasks_success_postgres_warehouse(
 def test_post_prefect_transformation_tasks_success_bigquery_warehouse(
     org_with_dbt_workspace,
 ):
-    """tests /tasks/transform/ success with bigquery warehouse"""
+    """tests POST /tasks/transform/ success with bigquery warehouse"""
     mock_orguser = Mock()
     mock_orguser.org = org_with_dbt_workspace
 
@@ -169,3 +226,18 @@ def test_post_prefect_transformation_tasks_success_bigquery_warehouse(
     OrgWarehouse.objects.create(org=org_with_dbt_workspace, wtype="bigquery")
 
     post_prefect_transformation_tasks(mock_request)
+
+
+def test_get_prefect_transformation_tasks_success(org_with_transformation_tasks):
+    """tests GET /tasks/transform/ success with bigquery warehouse"""
+    mock_orguser = Mock()
+    mock_orguser.org = org_with_transformation_tasks
+
+    mock_request = Mock()
+    mock_request.orguser = mock_orguser
+
+    resp = get_prefect_transformation_tasks(mock_request)
+
+    print(resp)
+
+    assert OrgTask.objects.filter(org=mock_orguser.org).count() == 5
