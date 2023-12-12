725237d62c1e74e8730347eb20a2fca4b4862b5a
Ishan <koradiaishan335@gmail.com>
2023-12-12 11:52:44 +0530

    get airbyte connection v1 test case

---
+++ ddpui/tests/api_tests/test_airbyte_api_v1.py
@@ -0,0 +1,125 @@
+import os
+import django
+
+from unittest.mock import Mock, patch, MagicMock
+import pytest
+from ninja.errors import HttpError
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+
+from ddpui.models.org_user import User, OrgUser
+from ddpui.models.org import Org, OrgPrefectBlock, OrgDataFlow, OrgWarehouse
+from ddpui.models.orgjobs import BlockLock, DataflowBlock
+from ddpui.models.org import OrgDbt, Org, OrgWarehouse, OrgPrefectBlockv1
+from ddpui.models.tasks import Task, OrgTask, OrgDataFlowv1, DataflowOrgTask
+from ddpui.api.client.airbyte_api import get_airbyte_connection_v1
+from ddpui.ddpairbyte.schema import AirbyteWorkspace
+from ddpui import ddpprefect
+
+pytestmark = pytest.mark.django_db
+
+
+# ================================================================================
+@pytest.fixture
+def org_without_workspace():
+    """a pytest fixture which creates an Org without an airbyte workspace"""
+    print("creating org_without_workspace")
+    org = Org.objects.create(airbyte_workspace_id=None, slug="test-org-slug")
+    yield org
+    print("deleting org_without_workspace")
+    org.delete()
+
+
+@pytest.fixture
+def org_with_workspace():
+    """a pytest fixture which creates an Org having an airbyte workspace"""
+    print("creating org_with_workspace")
+    org = Org.objects.create(
+        airbyte_workspace_id="FAKE-WORKSPACE-ID", slug="test-org-slug"
+    )
+    yield org
+    print("deleting org_with_workspace")
+    org.delete()
+
+
+# ================================================================================
+def test_get_airbyte_connection_v1_without_workspace(org_without_workspace):
+    """tests GET /v1/connections/{connection_id} failure with no workspace"""
+    mock_orguser = Mock()
+    mock_orguser.org = org_without_workspace
+
+    mock_request = Mock()
+    mock_request.orguser = mock_orguser
+
+    with pytest.raises(HttpError) as excinfo:
+        get_airbyte_connection_v1(mock_request, "fake-conn-id")
+
+    assert str(excinfo.value) == "create an airbyte workspace first"
+
+
+@patch.multiple(
+    "ddpui.ddpairbyte.airbyte_service",
+    get_connection=Mock(
+        return_value={
+            "name": "fake-conn",
+            "sourceId": "fake-source-id-1",
+            "connectionId": "fake-connection-id-1",
+            "destinationId": "fake-destination-id-1",
+            "catalogId": "fake-source-catalog-id-1",
+            "syncCatalog": "sync-catalog",
+            "namespaceDefinition": "namespace-definition",
+            "status": "conn-status",
+            "source": {"id": "fake-source-id-1", "sourceName": "fake-source-name-1"},
+            "destination": {
+                "id": "fake-destination-id-1",
+                "destinationName": "fake-destination-name-1",
+            },
+        }
+    ),
+)
+def test_get_airbyte_connection_v1_success(org_with_workspace):
+    mock_orguser = Mock()
+    mock_orguser.org = org_with_workspace
+
+    mock_request = Mock()
+    mock_request.orguser = mock_orguser
+
+    airbyte_task_config = {
+        "type": "airbyte",
+        "slug": "airbyte-sync",
+        "label": "AIRBYTE sync",
+        "command": None,
+    }
+    task = Task.objects.create(**airbyte_task_config)
+
+    org_task = OrgTask.objects.create(
+        task=task, org=mock_orguser.org, connection_id="fake-conn-id"
+    )
+
+    dataflow = OrgDataFlowv1.objects.create(
+        org=org_with_workspace,
+        name="test-deployment",
+        deployment_id="fake-deployment-id",
+        deployment_name="test-deployment",
+        cron=None,
+        dataflow_type="manual",
+    )
+
+    DataflowOrgTask.objects.create(dataflow=dataflow, orgtask=org_task)
+
+    result = get_airbyte_connection_v1(mock_request, "fake-conn-id")
+
+    assert result["name"] == "fake-conn"
+    assert result["source"]["id"] == "fake-source-id-1"
+    assert result["source"]["name"] == "fake-source-name-1"
+    assert result["destination"]["id"] == "fake-destination-id-1"
+    assert result["destination"]["name"] == "fake-destination-name-1"
+    assert result["catalogId"] == "fake-source-catalog-id-1"
+    assert result["syncCatalog"] == "sync-catalog"
+    assert result["status"] == "conn-status"
+    assert result["deploymentId"] == "fake-deployment-id"
+
+
+# ================================================================================
