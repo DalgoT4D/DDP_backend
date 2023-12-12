0dc684d43e996f5fbdf4cb2b74e005c0e83a1e14
Ishan <koradiaishan335@gmail.com>
2023-12-12 12:02:57 +0530

    get connections v1 test case

--- ddpui/tests/api_tests/test_airbyte_api_v1.py
+++ ddpui/tests/api_tests/test_airbyte_api_v1.py
@@ -14,7 +14,10 @@ from ddpui.models.org import Org, OrgPrefectBlock, OrgDataFlow, OrgWarehouse
 from ddpui.models.orgjobs import BlockLock, DataflowBlock
 from ddpui.models.org import OrgDbt, Org, OrgWarehouse, OrgPrefectBlockv1
 from ddpui.models.tasks import Task, OrgTask, OrgDataFlowv1, DataflowOrgTask
-from ddpui.api.client.airbyte_api import get_airbyte_connection_v1
+from ddpui.api.client.airbyte_api import (
+    get_airbyte_connection_v1,
+    get_airbyte_connections_v1,
+)
 from ddpui.ddpairbyte.schema import AirbyteWorkspace
 from ddpui import ddpprefect
 
@@ -80,6 +83,7 @@ def test_get_airbyte_connection_v1_without_workspace(org_without_workspace):
     ),
 )
 def test_get_airbyte_connection_v1_success(org_with_workspace):
+    """tests GET /v1/connections/{connection_id} success"""
     mock_orguser = Mock()
     mock_orguser.org = org_with_workspace
 
@@ -123,3 +127,97 @@ def test_get_airbyte_connection_v1_success(org_with_workspace):
 
 
 # ================================================================================
+def test_get_airbyte_connections_v1_without_workspace(org_without_workspace):
+    """tests GET /v1/connections failure with no workspace"""
+    mock_orguser = Mock()
+    mock_orguser.org = org_without_workspace
+
+    mock_request = Mock()
+    mock_request.orguser = mock_orguser
+
+    with pytest.raises(HttpError) as excinfo:
+        get_airbyte_connections_v1(mock_request)
+
+    assert str(excinfo.value) == "create an airbyte workspace first"
+
+
+@patch.multiple(
+    "ddpui.ddpairbyte.airbyte_service",
+    get_connection=Mock(
+        return_value={
+            "name": "fake-conn",
+            "sourceId": "fake-source-id-1",
+            "connectionId": "fake-connection-id-1",
+            "destinationId": "fake-destination-id-1",
+            "catalogId": "fake-source-catalog-id-1",
+            "syncCatalog": "sync-catalog",
+            "status": "conn-status",
+            "source": {"id": "fake-source-id-1", "name": "fake-source-name-1"},
+            "destination": {
+                "id": "fake-destination-id-1",
+                "name": "fake-destination-name-1",
+            },
+        }
+    ),
+)
+def test_get_airbyte_connections_success(org_with_workspace):
+    """tests GET /v1/connections success"""
+    mock_orguser = Mock()
+    mock_orguser.org = org_with_workspace
+
+    mock_request = Mock()
+    mock_request.orguser = mock_orguser
+
+    airbyte_task_config = {
+        "type": "airbyte",
+        "slug": "airbyte-sync",
+        "label": "AIRBYTE sync",
+        "command": None,
+    }
+    task = Task.objects.create(**airbyte_task_config)
+
+    org_task = OrgTask.objects.create(
+        task=task, org=mock_orguser.org, connection_id="fake-conn-id"
+    )
+
+    dataflow = OrgDataFlowv1.objects.create(
+        org=org_with_workspace,
+        name="test-deployment",
+        deployment_id="fake-deployment-id",
+        deployment_name="test-deployment",
+        cron=None,
+        dataflow_type="manual",
+    )
+
+    DataflowOrgTask.objects.create(dataflow=dataflow, orgtask=org_task)
+
+    result = get_airbyte_connections_v1(mock_request)
+    assert len(result) == 1
+
+    # assert result[0]["name"] == "fake-display-name"
+    # assert result[0]["blockId"] == "fake-block-id"
+    # assert result[0]["blockName"] == "fake-block-name"
+    # assert result[0]["blockData"]["connection_id"] == "fake-connection-id"
+    # assert result[0]["source"]["id"] == "fake-source-id-1"
+    # assert result[0]["source"]["sourceName"] == "fake-source-name-1"
+    # assert result[0]["destination"]["id"] == "fake-destination-id-1"
+    # assert result[0]["destination"]["destinationName"] == "fake-destination-name-1"
+    # assert result[0]["catalogId"] == "fake-source-catalog-id-1"
+    # assert result[0]["syncCatalog"] == "sync-catalog"
+    # assert result[0]["status"] == "conn-status"
+    # assert result[0]["deploymentId"] == "fake-deployment-id"
+    # assert result[0]["lastRun"] == {
+    #     "flow-run-id": "00000",
+    #     "startTime": 0,
+    #     "expectedStartTime": 0,
+    # }
+    # assert result[0]["lock"] is None
+    assert result[0]["name"] == "fake-conn"
+    assert result[0]["source"]["id"] == "fake-source-id-1"
+    assert result[0]["source"]["name"] == "fake-source-name-1"
+    assert result[0]["destination"]["id"] == "fake-destination-id-1"
+    assert result[0]["destination"]["name"] == "fake-destination-name-1"
+    assert result[0]["catalogId"] == "fake-source-catalog-id-1"
+    assert result[0]["syncCatalog"] == "sync-catalog"
+    assert result[0]["status"] == "conn-status"
+    assert result[0]["deploymentId"] == "fake-deployment-id"
