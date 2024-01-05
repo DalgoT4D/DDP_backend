049b418fed693e58e7b4ba2a7de0d9a67e55ee3d
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-01-05 16:27:51 +0530

    refining

--- ddpui/management/commands/fetch-airbyte-sync-stats.py
+++ ddpui/management/commands/fetch-airbyte-sync-stats.py
@@ -1,9 +1,11 @@
 """for each org fetch orchestration pipelines for airbyte sync"""
+from datetime import datetime
 from django.core.management.base import BaseCommand, CommandParser
 
 from ddpui.models.tasks import DataflowOrgTask
 from ddpui.ddpairbyte.airbyte_service import get_jobs_for_connection
-from ddpui.ddpairbyte.airbytehelpers import get_job_info_for_connection
+
+# from ddpui.ddpairbyte.airbytehelpers import get_job_info_for_connection
 
 
 class Command(BaseCommand):
@@ -12,16 +14,6 @@ class Command(BaseCommand):
     help = "Updates the mapping with the correct value of dataflow type in orgdataflow table"
 
     def add_arguments(self, parser: CommandParser) -> None:
-        parser.add_argument(
-            "--jobs",
-            action="store_true",
-            help="run airbyte_service.get_jobs_for_connection",
-        )
-        parser.add_argument(
-            "--job-info",
-            action="store_true",
-            help="run airbyte_helpers.get_job_info_for_connection",
-        )
         parser.add_argument("--org", type=str, help="org slug")
 
     def handle(self, *args, **options):
@@ -31,12 +23,11 @@ class Command(BaseCommand):
         ):
             if options["org"] and options["org"] != dfot.orgtask.org.slug:
                 continue
-            print(dfot.orgtask.org.slug, dfot.orgtask.connection_id)
-            if options["jobs"]:
-                jobs = get_jobs_for_connection(dfot.orgtask.connection_id)
-                print(jobs)
-            if options["job_info"]:
-                job_info = get_job_info_for_connection(
-                    dfot.orgtask.org, dfot.orgtask.connection_id
-                )
-                print(job_info)
+            org = dfot.orgtask.org
+            response = get_jobs_for_connection(dfot.orgtask.connection_id)
+            for job in response["jobs"]:
+                for attempt in job["attempts"]:
+                    timestamp = datetime.fromtimestamp(attempt["createdAt"])
+                    bytes_synced = attempt["bytesSynced"]
+                    yyyymmdd = timestamp.strftime("%Y-%m-%d")
+                    print(f"{org.slug:20} {yyyymmdd:20} {bytes_synced}")
