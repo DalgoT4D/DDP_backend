2c7d9fe0bc8d55fb77a12665b6aa41559642308d
Ishankoradia <ikoradia@umich.edu>
2024-02-12 12:02:00 +0530

    recreate orgdbt migration - sql path & config are nullable

--- ddpui/models/dbt_workflow.py
+++ ddpui/models/dbt_workflow.py
@@ -4,7 +4,7 @@ All the models related to UI for transformation will go here
 
 from django.db import models
 
-from ddpui.models.org import Org, OrgDbt
+from ddpui.models.org import OrgDbt
 
 
 class OrgDbtModel(models.Model):
@@ -13,8 +13,8 @@ class OrgDbtModel(models.Model):
     orgdbt = models.ForeignKey(OrgDbt, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     display_name = models.CharField(max_length=100)
-    sql_path = models.CharField(max_length=200)
-    config = models.JSONField(default=dict)
+    sql_path = models.CharField(max_length=200, null=True)
+    config = models.JSONField(null=True)
 
     def __str__(self) -> str:
-        return f"DbtModel[{self.orgdbt.id} | {self.orgdbt.project_dir}]"
+        return f"DbtModel[{self.name} | {self.orgdbt.project_dir}]"
