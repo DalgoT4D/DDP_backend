741ca9863eae240db26afe24fe4fc51cff2c26c4
Ishankoradia <ikoradia@umich.edu>
2024-02-26 11:06:31 +0530

    created migration for storing edge of dbt DAG

--- ddpui/models/dbt_workflow.py
+++ ddpui/models/dbt_workflow.py
@@ -8,7 +8,7 @@ from ddpui.models.org import OrgDbt
 
 
 class OrgDbtModel(models.Model):
-    """Docstring"""
+    """Model to store dbt models/sources in a project"""
 
     orgdbt = models.ForeignKey(OrgDbt, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
@@ -19,3 +19,18 @@ class OrgDbtModel(models.Model):
 
     def __str__(self) -> str:
         return f"DbtModel[{self.name} | {self.orgdbt.project_dir}]"
+
+
+class DbtEdge(models.Model):
+    """Edge to help generate the DAG of a dbt project. Edge is between two OrgDbtModel(s)"""
+
+    source = models.ForeignKey(
+        OrgDbtModel, on_delete=models.CASCADE, related_name="source"
+    )
+    target = models.ForeignKey(
+        OrgDbtModel, on_delete=models.CASCADE, related_name="target"
+    )
+    config = models.JSONField(null=True)
+
+    def __str__(self) -> str:
+        return f"DbtEdge[{self.source.schema}.{self.source.name} -> {self.target.schema}.{self.target.name}]"
