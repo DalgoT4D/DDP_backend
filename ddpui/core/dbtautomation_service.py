75c59676c5a61d0cd0e0ef59c468f97f89793a72
Ishankoradia <ikoradia@umich.edu>
2024-02-28 23:44:33 +0530

    updated the create_dbt_model_in_project to use the merge_operations for creating models

--- ddpui/core/dbtautomation_service.py
+++ ddpui/core/dbtautomation_service.py
@@ -12,12 +12,15 @@ from dbt_automation.operations.flattenairbyte import flatten_operation
 from dbt_automation.operations.flattenjson import flattenjson
 from dbt_automation.operations.mergetables import union_tables
 from dbt_automation.operations.regexextraction import regex_extraction
+from dbt_automation.operations.mergeoperations import merge_operations
 from dbt_automation.operations.syncsources import sync_sources
 from dbt_automation.utils.warehouseclient import get_client
 from dbt_automation.utils.dbtproject import dbtProject
 from dbt_automation.utils.dbtsources import read_sources
 
+from ddpui.schemas.dbt_workflow_schema import CompleteDbtModelPayload
 from ddpui.models.org import OrgDbt, OrgWarehouse
+from ddpui.models.dbt_workflow import OrgDbtModel, OrgDbtOperation
 from ddpui.utils import secretsmanager
 
 OPERATIONS_DICT = {
@@ -42,26 +45,36 @@ def _get_wclient(org_warehouse: OrgWarehouse):
 
 
 def create_dbt_model_in_project(
-    orgdbt: OrgDbt, org_warehouse: OrgWarehouse, op_type: str, config: dict
+    org_warehouse: OrgWarehouse,
+    orgdbt_model: OrgDbtModel,
+    payload: CompleteDbtModelPayload,
 ):
     """Create a dbt model in the project for an operation"""
 
     wclient = _get_wclient(org_warehouse)
-    if op_type not in OPERATIONS_DICT:
-        return None, "Operation not found"
 
-    # fetch input columns of the model
-    config["source_columns"] = wclient.get_table_columns(
-        config["dest_schema"], config["input"]["input_name"]
+    merge_config = {
+        "type": "mergeoperations",
+        "config": {
+            "output_name": payload.name,
+            "dest_schema": payload.dest_schema,
+            "input": "",  # TODO: need to update dbt_automation first
+            "operations": [],
+        },
+    }
+
+    operations = (
+        OrgDbtOperation.objects.filter(dbtmodel=orgdbt_model).order_by("seq").all()
     )
 
-    sql_file_path = OPERATIONS_DICT[op_type](
-        config=config,
-        warehouse=wclient,
-        project_dir=Path(orgdbt.project_dir) / "dbtrepo",
+    for operation in operations:
+        merge_config["config"]["operations"].append(operation.config)
+
+    model_sql_path = merge_operations(
+        merge_config, wclient, orgdbt_model.orgdbt.project_dir
     )
 
-    return str(sql_file_path), None
+    return str(model_sql_path), None
 
 
 def sync_sources_to_dbt(
@@ -85,3 +98,9 @@ def read_dbt_sources_in_project(orgdbt: OrgDbt):
     """Read the sources from .yml files in the dbt project"""
 
     return read_sources(Path(orgdbt.project_dir) / "dbtrepo")
+
+
+def get_table_columns(org_warehouse: OrgWarehouse, dbtmodel: OrgDbtModel):
+    """Get the columns of a table in a warehouse"""
+    wclient = _get_wclient(org_warehouse)
+    return wclient.get_table_columns(dbtmodel.schema, dbtmodel.name)
