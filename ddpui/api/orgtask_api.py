935452fd4c393735573d7a9f575135014bd64527
Ishankoradia <ikoradia@umich.edu>
2024-03-23 20:15:54 +0530

    pass flow run id along with task lock object

--- ddpui/api/orgtask_api.py
+++ ddpui/api/orgtask_api.py
@@ -293,6 +293,7 @@ def get_prefect_transformation_tasks(request):
                     {
                         "lockedBy": lock.locked_by.user.email,
                         "lockedAt": lock.locked_at,
+                        "flowRunId": lock.flow_run_id,
                     }
                     if lock
                     else None
