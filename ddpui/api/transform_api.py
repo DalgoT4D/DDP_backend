ab538b78da51a3cc92fd4734e0116e9cf87d782b
Ishankoradia <ikoradia@umich.edu>
2024-03-26 13:32:05 +0530

    refactor create operation api

--- ddpui/api/transform_api.py
+++ ddpui/api/transform_api.py
@@ -22,7 +22,9 @@ from ddpui.schemas.org_task_schema import DbtProjectSchema
 from ddpui.schemas.dbt_workflow_schema import (
     CreateDbtModelPayload,
     CompleteDbtModelPayload,
+    EditDbtOperationPayload,
 )
+from ddpui.core.transformfunctions import validate_operation_config
 
 from ddpui.core import dbtautomation_service
 from ddpui.core.dbtautomation_service import sync_sources_for_warehouse
@@ -196,42 +198,9 @@ def post_construct_dbt_model_operation(request, payload: CreateDbtModelPayload):
         dbtmodel=target_model
     ).count()
 
-    primary_input_model: OrgDbtModel = None  # the first input model
-    other_input_models: list[OrgDbtModel] = []
-    seq: list[int] = []
-    other_input_columns: list[list[str]] = []
-
-    logger.info(
-        f"Operations chained for the target model {target_model.uuid} : {current_operations_chained}"
+    final_config, all_input_models = validate_operation_config(
+        payload, target_model, is_multi_input_op, current_operations_chained
     )
-
-    if current_operations_chained == 0:
-        if not payload.input_uuid:
-            raise HttpError(422, "input is required")
-
-        model = OrgDbtModel.objects.filter(uuid=payload.input_uuid).first()
-        if not model:
-            raise HttpError(404, "input not found")
-
-        primary_input_model = model
-
-    if is_multi_input_op:  # multi input operation
-        if len(payload.other_inputs) == 0:
-            raise HttpError(422, "atleast 2 inputs are required for this operation")
-
-        payload.other_inputs.sort(key=lambda x: x.seq)
-
-        for other_input in payload.other_inputs:
-            model = OrgDbtModel.objects.filter(uuid=other_input.uuid).first()
-            if not model:
-                raise HttpError(404, "input not found")
-            seq.append(other_input.seq)
-            other_input_columns.append(other_input.columns)
-            other_input_models.append(model)
-
-    all_input_models = (
-        [primary_input_model] if primary_input_model else []
-    ) + other_input_models
 
     # we create edges only with tables/models at the start of the chain & not operation nodes
     if current_operations_chained == 0:
@@ -245,45 +214,8 @@ def post_construct_dbt_model_operation(request, payload: CreateDbtModelPayload):
                     to_node=target_model,
                 )
 
-    logger.info("passed all validation; moving to create operation")
-
-    # source columns or selected columns
-    # there will be atleast one input
-    OP_CONFIG = payload.config
-    OP_CONFIG["source_columns"] = payload.source_columns
-    OP_CONFIG["other_inputs"] = []
-
-    # in case of mutli input; send the rest of the inputs in the config; dbt_automation will handle the rest
-    for dbtmodel, seq, columns in zip(other_input_models, seq, other_input_columns):
-        OP_CONFIG["other_inputs"].append(
-            {
-                "input": {
-                    "input_type": dbtmodel.type,
-                    "input_name": dbtmodel.name,
-                    "source_name": dbtmodel.source_name,
-                },
-                "source_columns": columns,
-                "seq": seq,
-            }
-        )
-
-    input_config = {
-        "config": OP_CONFIG,
-        "type": payload.op_type,
-        "input_models": [
-            {
-                "uuid": str(model.uuid),
-                "name": model.name,
-                "display_name": model.display_name,
-                "source_name": model.source_name,
-                "schema": model.schema,
-                "type": model.type,
-            }
-            for model in all_input_models
-        ],
-    }
     output_cols = dbtautomation_service.get_output_cols_for_operation(
-        org_warehouse, payload.op_type, OP_CONFIG.copy()
+        org_warehouse, payload.op_type, final_config["config"].copy()
     )
     logger.info("creating operation")
 
@@ -291,7 +223,7 @@ def post_construct_dbt_model_operation(request, payload: CreateDbtModelPayload):
         dbtmodel=target_model,
         uuid=uuid.uuid4(),
         seq=current_operations_chained + 1,
-        config=input_config,
+        config=final_config,
         output_cols=output_cols,
     )
 
