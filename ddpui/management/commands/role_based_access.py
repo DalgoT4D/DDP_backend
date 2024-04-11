65587cc2f443f4f66569358f89f2fff8defffabb
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-04-11 13:44:45 +0530

    change to ACCOUNT_MANAGER_ROLE

--- ddpui/management/commands/role_based_access.py
+++ ddpui/management/commands/role_based_access.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 
 from ddpui.models.org_user import OrgUser
 from ddpui.models.role_based_access import Role
-from ddpui.auth import SUPER_ADMIN_ROLE
+from ddpui.auth import ACCOUNT_MANAGER_ROLE
 
 
 class Command(BaseCommand):
@@ -13,6 +13,6 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Update all orgusers to have role of Super admin
 
-        role = Role.objects.filter(slug=SUPER_ADMIN_ROLE).first()
+        role = Role.objects.filter(slug=ACCOUNT_MANAGER_ROLE).first()
         if role:
             OrgUser.objects.update(new_role=role)
