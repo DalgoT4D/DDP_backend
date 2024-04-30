3f8e408de3342d714472326f47743c4a5f90cd90
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-04-30 15:47:31 +0530

    send to platform admins even if ses_whitelisted_email is org.set

--- ddpui/utils/webhook_helpers.py
+++ ddpui/utils/webhook_helpers.py
@@ -94,7 +94,7 @@ def email_orgusers(org: Org, email_body: str):
     if org.ses_whitelisted_email:
         logger.info(f"sending email to {org.ses_whitelisted_email}")
         send_text_message(org.ses_whitelisted_email, subject, email_body)
-        return
+
     for orguser in OrgUser.objects.filter(
         org=org,
         role__in=[
