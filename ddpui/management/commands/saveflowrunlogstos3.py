1dbe86636a8114a820b22b0ea4d40cc9eaa475d5
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-05-26 12:48:15 +0530

    added airbyte logs

--- ddpui/management/commands/saveflowrunlogstos3.py
+++ ddpui/management/commands/saveflowrunlogstos3.py
@@ -2,7 +2,9 @@ import json
 import boto3
 from django.core.management.base import BaseCommand
 from ddpui.models.org import OrgDataFlowv1
+from ddpui.models.tasks import OrgTask
 from ddpui.ddpprefect import prefect_service
+from ddpui.ddpairbyte import airbyte_service
 
 
 class Command(BaseCommand):
@@ -34,9 +36,30 @@ class Command(BaseCommand):
                     )
                     print(f"Saved s3://dalgo-t4dai/{s3key}")
 
-    def fetch_airbyte_logs(self, org: str):
+    def fetch_airbyte_logs(self, org: str, s3):
         """fetches airbyte logs for and org"""
-        pass
+        for dataflow in OrgTask.objects.filter(
+            org__slug=org,
+            task__slug="airbyte-sync",
+            generated_by="system",
+        ):
+            connection_id = dataflow.connection_id
+            result = airbyte_service.get_jobs_for_connection(connection_id)
+            if len(result["jobs"]) == 0:
+                continue
+            for job in result["jobs"]:
+                job_info = airbyte_service.parse_job_info(job)
+                job_id = job_info["job_id"]
+                logs = airbyte_service.get_logs_for_job(job_id)
+                logs = logs["logs"]
+                if len(logs["logLines"]) > 0:
+                    s3key = f"logs/airbyte/{org}/{job_id}.json"
+                    s3.put_object(
+                        Bucket="dalgo-t4dai",
+                        Key=s3key,
+                        Body=json.dumps(logs),
+                    )
+                    print(f"Saved s3://dalgo-t4dai/{s3key}")
 
     def handle(self, *args, **options):
         # Your command logic goes here
@@ -48,3 +71,4 @@ class Command(BaseCommand):
             aws_secret_access_key=options["aws_secret"],
         )
         self.fetch_prefect_logs_from_deployments(org, s3)
+        self.fetch_airbyte_logs(org, s3)
