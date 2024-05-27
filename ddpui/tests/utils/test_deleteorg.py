a3203a20a6eed7f3aa33ba12a3f9c281e022be83
Ishankoradia <ikoradia@umich.edu>
2024-05-27 12:02:29 +0530

    update the delete org logic

--- ddpui/tests/utils/test_deleteorg.py
+++ ddpui/tests/utils/test_deleteorg.py
@@ -7,16 +7,14 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 django.setup()
 
-from ddpui.models.org import Org, OrgPrefectBlock, OrgWarehouse
+from ddpui.models.org import Org
 from ddpui.models.org_user import OrgUser, User
 
 from ddpui.utils.deleteorg import (
-    delete_prefect_shell_blocks,
     delete_dbt_workspace,
     delete_orgusers,
-    delete_airbyte_workspace,
+    delete_airbyte_workspace_v1,
 )
-from ddpui.ddpprefect import AIRBYTESERVER, AIRBYTECONNECTION, SHELLOPERATION
 
 pytestmark = pytest.mark.django_db
 
@@ -33,108 +31,10 @@ def org_with_workspace():
     org.delete()
 
 
-@patch.multiple(
-    "ddpui.ddpprefect.prefect_service",
-    delete_shell_block=Mock(),
-)
-def test_delete_prefect_shell_blocks(org_with_workspace):
-    """
-    deleting a prefect deployment should
-    - invoke delete_deployment_by_id
-    - delete the org-dataflow
-    """
-    OrgPrefectBlock.objects.create(org=org_with_workspace, block_type=SHELLOPERATION)
-    assert (
-        OrgPrefectBlock.objects.filter(
-            org=org_with_workspace, block_type=SHELLOPERATION
-        ).count()
-        == 1
-    )
-    delete_prefect_shell_blocks(org_with_workspace)
-    assert (
-        OrgPrefectBlock.objects.filter(
-            org=org_with_workspace, block_type=SHELLOPERATION
-        ).count()
-        == 0
-    )
-
-
 @patch.multiple("ddpui.ddpdbt.dbt_service", delete_dbt_workspace=Mock())
 def test_delete_dbt_workspace(org_with_workspace):
     """check that dbt_service.delete_dbt_workspace is called"""
     delete_dbt_workspace(org_with_workspace)
-
-
-@patch.multiple(
-    "ddpui.ddpprefect.prefect_service",
-    delete_airbyte_connection_block=Mock(),
-    delete_airbyte_server_block=Mock(),
-)
-@patch.multiple(
-    "ddpui.ddpairbyte.airbyte_service",
-    get_connections=Mock(
-        return_value={"connections": [{"connectionId": "fake-connection-id"}]}
-    ),
-    delete_connection=Mock(),
-    get_destinations=Mock(
-        return_value={"destinations": [{"destinationId": "fake-destination-id"}]}
-    ),
-    delete_destination=Mock(),
-    get_sources=Mock(return_value={"sources": [{"sourceId": "fake-source-id"}]}),
-    delete_source=Mock(),
-    delete_workspace=Mock(),
-)
-@patch.multiple("ddpui.utils.secretsmanager", delete_warehouse_credentials=Mock())
-def test_delete_airbyte_workspace(org_with_workspace):
-    """
-    delete connection blocks
-    delete server blocks
-    delete connections
-    delete destinations
-    delete sources
-    delete warehouse credentials
-    delete workspace
-    """
-    OrgPrefectBlock.objects.create(
-        org=org_with_workspace,
-        block_type=AIRBYTECONNECTION,
-        block_id="fake-conn-block-id",
-        block_name="fake-conn-block-name",
-    )
-    OrgPrefectBlock.objects.create(
-        org=org_with_workspace,
-        block_type=AIRBYTESERVER,
-        block_id="fake-srvr-block-id",
-        block_name="fake-srvr-block-name",
-    )
-    OrgWarehouse.objects.create(org=org_with_workspace)
-    assert (
-        OrgPrefectBlock.objects.filter(
-            org=org_with_workspace, block_type=AIRBYTECONNECTION
-        ).count()
-        == 1
-    )
-    assert (
-        OrgPrefectBlock.objects.filter(
-            org=org_with_workspace, block_type=AIRBYTESERVER
-        ).count()
-        == 1
-    )
-    assert OrgWarehouse.objects.filter(org=org_with_workspace).count() == 1
-    delete_airbyte_workspace(org_with_workspace)
-    assert (
-        OrgPrefectBlock.objects.filter(
-            org=org_with_workspace, block_type=AIRBYTECONNECTION
-        ).count()
-        == 0
-    )
-    assert (
-        OrgPrefectBlock.objects.filter(
-            org=org_with_workspace, block_type=AIRBYTESERVER
-        ).count()
-        == 0
-    )
-    assert OrgWarehouse.objects.filter(org=org_with_workspace).count() == 0
 
 
 def test_delete_orgusers(org_with_workspace):
--- ddpui/utils/deleteorg.py
+++ ddpui/utils/deleteorg.py
@@ -1,13 +1,15 @@
 import uuid
 from ninja.errors import HttpError
 from ddpui.models.org_user import Org, OrgUser, OrgUserRole
-from ddpui.models.org import OrgPrefectBlock, OrgWarehouse
+from ddpui.models.org import OrgWarehouse
+from ddpui.models.tasks import OrgTask, DataflowOrgTask
 from ddpui.models.org import OrgDataFlowv1, OrgPrefectBlockv1
 from ddpui.ddpairbyte import airbyte_service
 from ddpui.ddpprefect import prefect_service
-from ddpui.ddpprefect import AIRBYTESERVER, AIRBYTECONNECTION, SHELLOPERATION
+from ddpui.ddpprefect import AIRBYTESERVER
 from ddpui.ddpdbt import dbt_service
 from ddpui.utils import secretsmanager
+from ddpui.utils.constants import TASK_AIRBYTESYNC, TASK_AIRBYTERESET
 
 from ddpui.utils.custom_logger import CustomLogger
 
@@ -23,120 +25,51 @@ def is_valid_uuid(s):
         return False
 
 
-def delete_prefect_shell_blocks(org: Org):  # skipcq: PYL-R0201
-    """fetches and deletes all prefect blocks for this org"""
-    logger.info("=========== OrgPrefectBlock: Shell Operations ===========")
-    for block in OrgPrefectBlock.objects.filter(org=org, block_type=SHELLOPERATION):
-        logger.info(
-            "%s %s %s",
-            block.block_name,
-            block.display_name,
-            block.command,
-        )
-        prefect_service.delete_shell_block(block.block_id)
-        block.delete()
-
-
 def delete_dbt_workspace(org: Org):  # skipcq: PYL-R0201
     """deletes the dbt workspace"""
     dbt_service.delete_dbt_workspace(org)
 
 
-def delete_airbyte_workspace(org: Org):  # skipcq: PYL-R0201
-    """
-    deletes airbyte sources, destinations, connections
-    deletes airbyte server and connection blocks in prefect
-    """
-    logger.info("=========== OrgPrefectBlock: Airbyte Connections ===========")
-    for block in OrgPrefectBlock.objects.filter(org=org, block_type=AIRBYTECONNECTION):
-        logger.info(
-            "%s %s %s %s %s",
-            block.block_name,
-            block.block_type,
-            block.display_name,
-            block.dbt_target_schema,
-            block.command,
-        )
-        prefect_service.delete_airbyte_connection_block(block.block_id)
-        block.delete()
-
-    logger.info("=========== OrgPrefectBlock: Airbyte Server(s) ===========")
-    for block in OrgPrefectBlock.objects.filter(org=org, block_type=AIRBYTESERVER):
-        logger.info(
-            "%s %s %s %s %s",
-            block.block_name,
-            block.block_type,
-            block.display_name,
-            block.dbt_target_schema,
-            block.command,
-        )
-        prefect_service.delete_airbyte_server_block(block.block_id)
-        block.delete()
-
-    if is_valid_uuid(org.airbyte_workspace_id):
-        for connection in airbyte_service.get_connections(org.airbyte_workspace_id)[
-            "connections"
-        ]:
-            logger.info("deleting connection in Airbyte " + connection["connectionId"])
-            airbyte_service.delete_connection(
-                org.airbyte_workspace_id, connection["connectionId"]
-            )
-
-        for destination in airbyte_service.get_destinations(org.airbyte_workspace_id)[
-            "destinations"
-        ]:
-            logger.info(
-                "deleting destination in Airbyte " + destination["destinationId"]
-            )
-            airbyte_service.delete_destination(
-                org.airbyte_workspace_id, destination["destinationId"]
-            )
-
-        for source in airbyte_service.get_sources(org.airbyte_workspace_id)["sources"]:
-            logger.info("deleting source in Airbyte " + source["sourceId"])
-            airbyte_service.delete_source(org.airbyte_workspace_id, source["sourceId"])
-
-    for warehouse in OrgWarehouse.objects.filter(org=org):
-        secretsmanager.delete_warehouse_credentials(warehouse)
-        warehouse.delete()
-
-    if is_valid_uuid(org.airbyte_workspace_id):
-        try:
-            airbyte_service.delete_workspace(org.airbyte_workspace_id)
-        except Exception:
-            pass
-
-
 def delete_warehouse_v1(org: Org):  # skipcq: PYL-R0201
     """
-    deletes airbyte sources, destinations, connections
-    deletes airbyte server and connection blocks in prefect
+    deletes airbyte/django/prefect destinations, connections
     """
-    logger.info("=========== OrgPrefectBlock: Airbyte Connections ===========")
-    for block in OrgPrefectBlockv1.objects.filter(
-        org=org, block_type=AIRBYTECONNECTION
+    logger.info(
+        "=============== Remove Dataflows/deployments: connections & reset connections ==========="
+    )
+    for dataflow_orgtask in DataflowOrgTask.objects.filter(
+        orgtask__org=org, orgtask__task__slug__in=[TASK_AIRBYTESYNC, TASK_AIRBYTERESET]
     ):
-        logger.info(
-            "%s %s %s %s %s",
-            block.block_name,
-            block.block_type,
-            block.display_name,
-            block.dbt_target_schema,
-            block.command,
-        )
+        dataflow = dataflow_orgtask.dataflow
         try:
-            prefect_service.delete_airbyte_connection_block(block.block_id)
-            logger.info(f"delete connecion block id - {block.block_id}")
-        except Exception:  # skipcq PYL-W0703
-            logger.error(
-                "failed to delete %s airbyte-connection-block %s in prefect, deleting from OrgPrefectBlockv1",
-                org.slug,
-                block.block_id,
-            )
-        block.delete()
+            prefect_service.delete_deployment_by_id(dataflow.deployment_id)
+        except HttpError:
+            pass
+        dataflow.delete()
+        logger.info(f"Deleted deployment - {dataflow.deployment_id}")
 
-    logger.info("FINISHED Deleting prefect connection blocks")
+    logger.info(
+        "================ Remove OrgTasks:connections & reset connections ==========="
+    )
+    for org_task in OrgTask.objects.filter(
+        org=org, task__slug__in=[TASK_AIRBYTESYNC, TASK_AIRBYTERESET]
+    ).all():
+        try:
+            if org_task.connection_id:
+                logger.info("deleting connection in Airbyte " + org_task.connection_id)
+                airbyte_service.delete_connection(
+                    org.airbyte_workspace_id, org_task.connection_id
+                )
+                logger.info(
+                    f"deleted connection in Airbyte - {connection['connectionId']}"
+                )
+        except Exception:
+            pass
+        org_task.delete()
 
+    logger.info(
+        "================ Remove remaining airbyte connections & destinations (warehouse) ==========="
+    )
     if is_valid_uuid(org.airbyte_workspace_id):
         for connection in airbyte_service.get_connections(org.airbyte_workspace_id)[
             "connections"
@@ -160,32 +93,19 @@ def delete_warehouse_v1(org: Org):  # skipcq: PYL-R0201
                 f"deleted destination in Airbyte - {destination['destinationId']}"
             )
 
-    logger.info("Deleting django warehouse and the credentials in secrets manager")
+    logger.info(
+        "================ Remove django warehouse and the credentials in secrets manager ==========="
+    )
     for warehouse in OrgWarehouse.objects.filter(org=org):
         secretsmanager.delete_warehouse_credentials(warehouse)
         warehouse.delete()
 
-    # delete dbt workspace and blocks
-    dbt_service.delete_dbt_workspace(org)
-
-    # delete dataflows
-    logger.info("Deleting data flows")
-    for data_flow in OrgDataFlowv1.objects.filter(org=org):
-        try:
-            prefect_service.delete_deployment_by_id(data_flow.deployment_id)
-        except HttpError:
-            pass
-        data_flow.delete()
-        logger.info(f"Deleted deployment - {data_flow.deployment_id}")
-    logger.info("FINISHED Deleting data flows")
-
 
 def delete_airbyte_workspace_v1(org: Org):  # skipcq: PYL-R0201
     """
-    deletes airbyte sources, destinations, connections
-    deletes airbyte server and connection blocks in prefect
+    deletes airbyte sources and the workspace
+    deletes airbyte/django/prefect server blocks
     """
-    delete_warehouse_v1(org)
 
     if is_valid_uuid(org.airbyte_workspace_id):
         for source in airbyte_service.get_sources(org.airbyte_workspace_id)["sources"]:
@@ -196,6 +116,15 @@ def delete_airbyte_workspace_v1(org: Org):  # skipcq: PYL-R0201
             airbyte_service.delete_workspace(org.airbyte_workspace_id)
         except Exception:
             pass
+
+    for block in OrgPrefectBlockv1.objects.filter(
+        org=org, block_type=AIRBYTESERVER
+    ).all():
+        try:
+            prefect_service.prefect_delete_a_block(block.block_id)
+        except Exception:
+            pass
+        block.delete()
 
 
 def delete_orgusers(org: Org):  # skipcq: PYL-R0201
@@ -223,8 +152,7 @@ def delete_one_org(org: Org):
     """delete one org"""
     delete_dbt_workspace(org)
     if org.airbyte_workspace_id:
-        delete_airbyte_workspace(org)
         delete_airbyte_workspace_v1(org)
-    delete_prefect_shell_blocks(org)
+    delete_warehouse_v1(org)
     delete_orgusers(org)
     org.delete()
