cb4c3192e3c6d09fad7af88f41e14fee1bad6bd6
Ishankoradia <ikoradia@umich.edu>
2024-06-11 15:21:57 +0530

    added a fixture

--- ddpui/tests/core/test_orgtaskfunctions.py
+++ ddpui/tests/core/test_orgtaskfunctions.py
@@ -1,15 +1,37 @@
 import os
-import json
-from pathlib import Path
+import django
+from django.core.management import call_command
 from django.apps import apps
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
+django.setup()
+
 
 import pytest
 from ddpui.models.org import Org
 from ddpui.core.orgtaskfunctions import get_edr_send_report_task
 from ddpui.models.tasks import Task
-from ddpui.tests.api_tests.test_pipeline_api import seed_master_tasks_db
 
 pytestmark = pytest.mark.django_db
+
+
+@pytest.fixture(scope="session")
+def seed_master_tasks_db(django_db_setup, django_db_blocker):
+    with django_db_blocker.unblock():
+        # Run the loaddata command to load the fixture
+        call_command("loaddata", "tasks.json")
+
+
+# ================================================================================
+
+
+def test_seed_master_tasks(seed_master_tasks_db):
+    """a test to seed the database"""
+    assert Task.objects.count() == 10
+
+
+# ================================================================================
 
 
 def test_get_edr_send_report_task(seed_master_tasks_db):
