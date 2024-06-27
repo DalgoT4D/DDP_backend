d935bfce9bb18058b04549356d9af18aafde8261
Ishankoradia <ikoradia@umich.edu>
2024-06-28 06:58:41 +0700

    identify the attempt number while parsing the job info & use it fetch logs

--- ddpui/ddpairbyte/airbyte_service.py
+++ ddpui/ddpairbyte/airbyte_service.py
@@ -917,8 +917,10 @@ def parse_job_info(jobinfo: dict) -> dict:
         "recordsCommitted": 0,
         "totalTimeInSeconds": 0,
     }
+    retval["attempt_no"] = 0
     for attempt in jobinfo["attempts"]:
         if attempt["status"] == "succeeded":
+            retval["attempt_no"] = attempt["id"]
             retval["recordsSynced"] = attempt["recordsSynced"]
             retval["bytesSynced"] = nice_bytes(attempt["bytesSynced"])
             retval["recordsEmitted"] = attempt["totalStats"]["recordsEmitted"]
