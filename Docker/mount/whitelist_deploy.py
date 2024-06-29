c950bb0de7729aa37cbf7d0af680e35afbf795b4
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-06-29 09:02:14 +0530

    skip-cq

--- Docker/mount/whitelist_deploy.py
+++ Docker/mount/whitelist_deploy.py
@@ -6,7 +6,7 @@ DEMO_WHITELIST_SOURCES = [
         "type": "Google Sheets",
         "config": {
             "row_batch_size": 500,
-            "spreadsheet_id": "https://docs.google.com/spreadsheets/d/18GqjB6nFBxZbAPnk6UOAvERE3iMfi4AkidjV6HkFKQk/edit#gid=0",
+            "spreadsheet_id": "https://docs.google.com/spreadsheets/d/18GqjB6nFBxZbAPnk6UOAvERE3iMfi4AkidjV6HkFKQk/edit#gid=0",  # skip-cq: FLK-E501
             "credentials": {
                 "auth_type": "Service",
                 "service_account_info": json.dumps(
