901354d6b2b6710deb3c8ea33dfcec87b91cb044
Ishankoradia <ikoradia@umich.edu>
2024-06-30 12:56:25 +0700

    boolean column insights

--- ddpui/tests/core/datainsights/insights/test_boolean_insights.py
+++ ddpui/tests/core/datainsights/insights/test_boolean_insights.py
@@ -11,18 +11,18 @@ django.setup()
 
 import pytest
 
-from ddpui.datainsights.insights.numeric_type.queries import DataStats
+from ddpui.datainsights.insights.boolean_type.queries import DataStats
 from ddpui.datainsights.insights.insight_interface import (
     MAP_TRANSLATE_TYPES,
     TranslateColDataType,
 )
-from ddpui.datainsights.insights.numeric_type.numeric_insight import NumericColInsights
+from ddpui.datainsights.insights.boolean_type.boolean_insights import BooleanColInsights
 
 
 @pytest.fixture
-def numeric_payload():
+def boolean_payload():
     """
-    Fixture to create an instance of NumericColInsights
+    Fixture to create an instance of BooleanColInsights
     """
     columns = [
         {
@@ -45,9 +45,9 @@ def numeric_payload():
 
 
 @pytest.fixture
-def invalid_numeric_payload():
+def invalid_boolean_payload():
     """
-    Fixture to create an instance of NumericColInsights
+    Fixture to create an instance of BooleanColInsights
     """
     columns = []
     db_table = "test_table"
@@ -63,13 +63,13 @@ def invalid_numeric_payload():
     }
 
 
-def test_numeric_insights_num_of_queries(numeric_payload):
+def test_numeric_insights_num_of_queries(boolean_payload):
     """
     Success test
     - checking no of queries to run
     - checking the type of queries
     """
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
 
     assert len(obj.insights) == 1
     assert isinstance(obj.insights[0], DataStats)
@@ -78,73 +78,66 @@ def test_numeric_insights_num_of_queries(numeric_payload):
 # ============================== Data stats query tests ================================
 
 
-def test_data_stats_query_when_no_col_specified(invalid_numeric_payload):
+def test_data_stats_query_when_no_col_specified(invalid_boolean_payload):
     """Failure case: numeric insights should have atleast one column to generate sql"""
-    obj = NumericColInsights(**invalid_numeric_payload)
+    obj = BooleanColInsights(**invalid_boolean_payload)
 
     with pytest.raises(ValueError):
         obj.insights[0].generate_sql()
 
 
-def test_data_stats_query_data_type(numeric_payload):
-    obj = NumericColInsights(**numeric_payload)
+def test_data_stats_query_data_type(boolean_payload):
+    obj = BooleanColInsights(**boolean_payload)
 
     data_stats_insights = obj.insights[0]
-    assert data_stats_insights.query_data_type() == TranslateColDataType.NUMERIC
+    assert data_stats_insights.query_data_type() == TranslateColDataType.BOOL
 
 
-def test_data_stats_query_parse_results(numeric_payload):
+def test_data_stats_query_parse_results(boolean_payload):
     """Success test case of parsing the results of the query"""
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
 
     data_stats_insights = obj.insights[0]
 
     mock_results = [
         {
-            "mean": Decimal(1.2),
-            "median": Decimal(1.5),
-            "mode": Decimal(2),
-            "other_modes": [],
+            "countTrue": 50,
+            "countFalse": 20,
         }
     ]
     output = data_stats_insights.parse_results(mock_results)
 
     assert data_stats_insights.columns[0].name in output
-    assert output[data_stats_insights.columns[0].name]["mean"] == 1.2
-    assert output[data_stats_insights.columns[0].name]["median"] == 1.5
-    assert output[data_stats_insights.columns[0].name]["mode"] == 2
+    assert output[data_stats_insights.columns[0].name]["countTrue"] == 50
+    assert output[data_stats_insights.columns[0].name]["countFalse"] == 20
 
 
-def test_data_stats_query_parse_results_failure(numeric_payload):
+def test_data_stats_query_parse_results_failure(boolean_payload):
     """Failure test case of parsing the results of the query; missing keys"""
 
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
 
     data_stats_insights = obj.insights[0]
 
     mock_results = [
         {
-            "mean": Decimal(1.2),
-            "median": Decimal(1.5),
-            "other_modes": [],
+            "some_wrong_key": 50,
         }
     ]
     with pytest.raises(KeyError):
         data_stats_insights.parse_results(mock_results)
 
 
-def test_data_stats_query_validate_results(numeric_payload):
+def test_data_stats_query_validate_results(boolean_payload):
     """Success test case of validating the parsed results of the query"""
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
 
     data_stats_insights = obj.insights[0]
 
     mock_results = [
         {
-            "mean": Decimal(1.2),
-            "median": Decimal(1.5),
-            "mode": Decimal(2),
-            "other_modes": [],
+            "countTrue": 50,
+            "countFalse": 20,
         }
     ]
     mock_output = data_stats_insights.parse_results(mock_results)
@@ -152,23 +145,23 @@ def test_data_stats_query_validate_results(numeric_payload):
     assert data_stats_insights.validate_query_results(result_to_be_validated)
 
 
-def test_data_stats_query_uniqueness_of_query_id(numeric_payload):
+def test_data_stats_query_uniqueness_of_query_id(boolean_payload):
     """Different payload should generated different query ids & hence a different hash"""
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
     data_stats_insights = obj.insights[0]
 
     # for a differnet col under same table, schema; a new query id (hash) should be generated
-    numeric_payload["columns"][0]["name"] = "col2"
+    boolean_payload["columns"][0]["name"] = "col2"
 
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
     data_stats_insights1 = obj.insights[0]
 
     assert data_stats_insights.query_id() != data_stats_insights1.query_id()
 
     # if i add a filter, the query id should change
-    numeric_payload["filter_"] = {"condition": 1}
+    boolean_payload["filter_"] = {"condition": 1}
 
-    obj = NumericColInsights(**numeric_payload)
+    obj = BooleanColInsights(**boolean_payload)
     data_stats_insights2 = obj.insights[0]
 
     assert data_stats_insights1.query_id() != data_stats_insights2.query_id()
