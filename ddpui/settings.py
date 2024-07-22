3b19b78f3bbcfa8f79dbce7045654debd075697a
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-07-22 22:36:51 +0530

    SENTRY_TSR and SENTRY_PSR

--- ddpui/settings.py
+++ ddpui/settings.py
@@ -26,11 +26,11 @@ sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
+    traces_sample_rate=float(os.getenv("SENTRY_TSR", "1.0")),
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
+    profiles_sample_rate=float(os.getenv("SENTRY_PSR", "1.0")),
 )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
