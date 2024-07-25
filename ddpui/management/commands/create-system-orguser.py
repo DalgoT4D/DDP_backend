1fa2cc9412eda7650a8b8a0086d213994a33fbe7
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-07-25 22:46:09 +0530

    guard against duplicate creation call from docker entrypoint

--- ddpui/management/commands/create-system-orguser.py
+++ ddpui/management/commands/create-system-orguser.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
-from ddpui.models.org import Org, OrgWarehouse
 from ddpui.models.org_user import OrgUser, OrgUserRole
 from ddpui.utils.custom_logger import CustomLogger
 from ddpui.utils.constants import SYSTEM_USER_EMAIL
@@ -24,11 +23,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """create orguser with null org and auth user with no password"""
-        user = User.objects.create(
-            email=SYSTEM_USER_EMAIL,
-            username=SYSTEM_USER_EMAIL,
-            password="",
-        )
-        logger.info("create auth user")
-        OrgUser.objects.create(user=user, org=None, role=OrgUserRole.ACCOUNT_MANAGER)
-        logger.info(f"created system orguser")
+        user = User.objects.filter(email=SYSTEM_USER_EMAIL).first()
+        if user is None:
+            user = User.objects.create(
+                email=SYSTEM_USER_EMAIL,
+                username=SYSTEM_USER_EMAIL,
+                password="",
+            )
+            logger.info("created auth user")
+        orguser = OrgUser.objects.filter(user=user).first()
+        if orguser is None:
+            OrgUser.objects.create(
+                user=user, org=None, role=OrgUserRole.ACCOUNT_MANAGER
+            )
+            logger.info("created system orguser")
