aa2fc0895b8d6291bc21ef3c547cd3b3b34453bd
Ishankoradia <ikoradia@umich.edu>
2024-08-07 15:47:27 -0500

    create a delpoyment for dbt seed task

--- ddpui/api/orgtask_api.py
+++ ddpui/api/orgtask_api.py
@@ -53,6 +53,7 @@ from ddpui.utils.constants import (
     TASK_GITPULL,
     TRANSFORM_TASKS_SEQ,
     TASK_GENERATE_EDR,
+    TASK_SEED,
 )
 from ddpui.core.orgtaskfunctions import get_edr_send_report_task
 from ddpui.core.pipelinefunctions import (
@@ -131,7 +132,7 @@ def post_orgtask(request, payload: CreateOrgTaskPayload):
     )
 
     dataflow = None
-    if task.slug == TASK_DBTRUN:
+    if task.slug in [TASK_DBTRUN, TASK_SEED]:
         dbt_project_params, error = gather_dbt_project_params(orguser.org)
         if error:
             raise HttpError(400, error)
