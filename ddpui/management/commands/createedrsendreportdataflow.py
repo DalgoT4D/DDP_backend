e8a8c71409bed96a90173f8cd75b089e161084cf
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-08-11 08:35:48 +0530

    refactor and simplify

--- ddpui/management/commands/createedrsendreportdataflow.py
+++ ddpui/management/commands/createedrsendreportdataflow.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from django.core.management.base import BaseCommand
 
 from ddpui.models.org import Org, OrgDataFlowv1, OrgDbt
-from ddpui.models.tasks import DataflowOrgTask
+from ddpui.models.tasks import OrgTask, DataflowOrgTask
 from ddpui.core.orgtaskfunctions import get_edr_send_report_task
 from ddpui.core.pipelinefunctions import setup_edr_send_report_task_config
 from ddpui.core.dbtfunctions import gather_dbt_project_params
@@ -23,10 +23,58 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("org", type=str, help="Org slug")
-        parser.add_argument(
-            "--schedule", choices=["manual", "orchestrate"], default="manual"
-        )
         parser.add_argument("--cron", type=str, default="0 0 * * *")
+
+    def create_dataflow(self, org: Org, org_task: OrgTask, cron: str):
+        """create the DataflowOrgTask for the orgtask"""
+        print("No existing dataflow found for generate-edr, creating one")
+
+        dbt_project_params, error = gather_dbt_project_params(org)
+        if error:
+            print(error)
+            return None
+
+        dbt_env_dir = Path(org.dbt.dbt_venv)
+
+        task_config = setup_edr_send_report_task_config(
+            org_task, dbt_project_params.project_dir, dbt_env_dir
+        )
+        print("task_config for deployment")
+        print(task_config.to_json())
+
+        hash_code = generate_hash_id(8)
+        deployment_name = (
+            f"pipeline-{org_task.org.slug}-{org_task.task.slug}-{hash_code}"
+        )
+        print(f"creating deployment {deployment_name}")
+
+        dataflow = prefect_service.create_dataflow_v1(
+            PrefectDataFlowCreateSchema3(
+                deployment_name=deployment_name,
+                flow_name=deployment_name,
+                orgslug=org_task.org.slug,
+                deployment_params={
+                    "config": {
+                        "tasks": [task_config.to_json()],
+                        "org_slug": org_task.org.slug,
+                    }
+                },
+            ),
+            MANUL_DBT_WORK_QUEUE,
+        )
+
+        print(
+            f"creating OrgDataFlowv1 named {dataflow['deployment']['name']} with deployment_id {dataflow['deployment']['id']}"
+        )
+        orgdataflow = OrgDataFlowv1.objects.create(
+            org=org,
+            name=dataflow["deployment"]["name"],
+            deployment_name=dataflow["deployment"]["name"],
+            deployment_id=dataflow["deployment"]["id"],
+            dataflow_type="manual",  # we dont want it to show in flows/pipelines page
+            cron=cron,
+        )
+        return orgdataflow
 
     def handle(self, *args, **options):
 
@@ -47,54 +95,6 @@ class Command(BaseCommand):
 
         dataflow_orgtask = DataflowOrgTask.objects.filter(orgtask=org_task).first()
 
-        dataflow = dataflow_orgtask.dataflow if dataflow_orgtask else None
-        if dataflow is None:
-            print("No existing dataflow found for generate-edr, creating one")
-
-            dbt_project_params, error = gather_dbt_project_params(org)
-            if error:
-                print(error)
-                return
-
-            dbt_env_dir = Path(org.dbt.dbt_venv)
-
-            task_config = setup_edr_send_report_task_config(
-                org_task, dbt_project_params.project_dir, dbt_env_dir
-            )
-            print("task_config for deployment")
-            print(task_config.to_json())
-
-            hash_code = generate_hash_id(8)
-            deployment_name = (
-                f"pipeline-{org_task.org.slug}-{org_task.task.slug}-{hash_code}"
-            )
-            print(f"creating deployment {deployment_name}")
-
-            dataflow = prefect_service.create_dataflow_v1(
-                PrefectDataFlowCreateSchema3(
-                    deployment_name=deployment_name,
-                    flow_name=deployment_name,
-                    orgslug=org_task.org.slug,
-                    deployment_params={
-                        "config": {
-                            "tasks": [task_config.to_json()],
-                            "org_slug": org_task.org.slug,
-                        }
-                    },
-                ),
-                MANUL_DBT_WORK_QUEUE,
-            )
-
-            print(
-                f"creating `{options['schedule']}` OrgDataFlowv1 named {dataflow['deployment']['name']} with deployment_id {dataflow['deployment']['id']}"
-            )
-            data_flow = OrgDataFlowv1.objects.create(
-                org=org,
-                name=dataflow["deployment"]["name"],
-                deployment_name=dataflow["deployment"]["name"],
-                deployment_id=dataflow["deployment"]["id"],
-                dataflow_type="manual",  # we dont want it to show in flows/pipelines page
-                cron=options["cron"] if options["schedule"] == "orchestrate" else None,
-            )
-
-            DataflowOrgTask.objects.create(dataflow=data_flow, orgtask=org_task)
+        if dataflow_orgtask is None:
+            dataflow = self.create_dataflow(org, org_task, options["cron"])
+            DataflowOrgTask.objects.create(dataflow=dataflow, orgtask=org_task)
