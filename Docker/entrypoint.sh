05edca5a6636a2bde86a76d311125f54427da6d6
Ishankoradia <ikoradia@umich.edu>
2024-10-17 17:14:34 +0530

    same env for all docker services; added celery worker and update entrypoint.sh

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -4,29 +4,19 @@
 
 # create case of different containers
 case "$1" in
-    celery)
+    worker)
         echo "Starting celery worker"
         celery -A ddpui worker -n ddpui
         ;;
-
     beat)
         echo "Starting celery beat"
         celery -A ddpui beat --schedule=/data/celerybeat-schedule --loglevel=error --max-interval 60
         ;;
     backend)
         echo "Starting backend"
-        
-
-        echo "Create first user ${FIRST_USER_EMAIL} in organization ${FIRST_ORG_NAME}"
-        python manage.py createorganduser ${FIRST_ORG_NAME} ${FIRST_USER_EMAIL} ${FIRST_USER_PASSWORD} --role ${FIRST_USER_ROLE}
-
-        echo "Create system user"
-        python manage.py create-system-orguser
 
         # Start server
         echo "Starting server"
-
-
         python3 -m gunicorn -b 0.0.0.0:8002 ddpui.wsgi \
             --capture-output \
             --log-config /usr/src/backend/gunicorn-log.conf \
@@ -38,6 +28,12 @@ case "$1" in
 
         echo "Seed database"
         python manage.py loaddata seed/*.json
+
+        echo "Create first user ${FIRST_USER_EMAIL} in organization ${FIRST_ORG_NAME}"
+        python manage.py createorganduser ${FIRST_ORG_NAME} ${FIRST_USER_EMAIL} ${FIRST_USER_PASSWORD} --role ${FIRST_USER_ROLE}
+
+        echo "Create system user if it does not exist"
+        python manage.py create-system-orguser
         ;;
     *)
         exec "$@"
