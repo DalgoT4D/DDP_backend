a9b70a4287ff1ec0435836efbe074a8789882acc
himanshudube97 <himanshu.dube13@gmail.com>
2024-12-11 03:08:56 +0530

    Added celery task and webhook for schema catalog, need to check for errors and also refactoring and testing

--- ddpui/websockets/airbyte_consumer.py
+++ ddpui/websockets/airbyte_consumer.py
@@ -1,6 +1,10 @@
 import json
 
+from ddpui.celeryworkers.tasks import get_schema_catalog_task
+from ddpui.models.org_user import OrgUser
+from ddpui.models.tasks import TaskProgressHashPrefix, TaskProgressStatus
 from ddpui.utils.custom_logger import CustomLogger
+from ddpui.utils.singletaskprogress import SingleTaskProgress
 from ddpui.websockets.schemas import WebsocketResponse, WebsocketResponseStatus
 from ddpui.ddpairbyte import airbyte_service, airbytehelpers
 from ddpui.models.org import OrgType
@@ -11,6 +15,8 @@ from ddpui.ddpairbyte.schema import (
     AirbyteDestinationUpdateCheckConnection,
 )
 from ddpui.websockets import BaseConsumer
+
+import time
 
 logger = CustomLogger("ddpui")
 
@@ -123,3 +129,96 @@ class DestinationCheckConnectionConsumer(BaseConsumer):
                 status=WebsocketResponseStatus.SUCCESS,
             )
         )
+
+
+class SchemaCatalogConsumer(BaseConsumer):
+    """websocket for checking source schema_catalog"""
+
+    def websocket_receive(self, message):
+        logger.info("Recieved the message from client, get schema_catalog inside the connection")
+        payload = json.loads(message["text"])
+
+        orguser: OrgUser = self.orguser
+        if orguser.org.airbyte_workspace_id is None:
+            self.respond(
+                WebsocketResponse(
+                    data={},
+                    message="Create an airbyte workspace first.",
+                    status=WebsocketResponseStatus.ERROR,
+                )
+            )
+            return
+
+        if "sourceId" not in payload:
+            self.respond(
+                WebsocketResponse(
+                    data={},
+                    message="SourceId is required in the payload",
+                    status=WebsocketResponseStatus.ERROR,
+                )
+            )
+            return
+        source_id = payload.get("sourceId", None)
+
+        # creating a key and checking if it exists or not
+        task_key = f"{TaskProgressHashPrefix.SOURCE_SCHEMA_CATALOG}-{orguser.org.slug}-{source_id}"  # change this schema change to catalog
+        print(task_key, "taskkey ")
+        task_progress = SingleTaskProgress.fetch(task_key)
+
+        if task_progress is not None:
+            polling_celery(self, task_key)
+            return
+
+        # creating a celery task now #.delay gives this task to celery
+        get_schema_catalog_task.delay(task_key, str(orguser.org.airbyte_workspace_id), source_id)
+        time.sleep(2)
+        polling_celery(self, task_key)
+
+
+def polling_celery(self, task_key):
+    task_progress = SingleTaskProgress.fetch(task_key)
+    if task_progress is None:
+        self.respond(
+            WebsocketResponse(
+                data={},
+                message="No Task of this task_key found",
+                status=WebsocketResponseStatus.ERROR,
+            )
+        )
+        return
+
+    last_status = task_progress[-1]["status"]
+
+    while last_status == TaskProgressStatus.RUNNING:
+        task_progress = SingleTaskProgress.fetch(task_key)
+        last_status = None if task_progress is None else task_progress[-1]["status"]
+        time.sleep(2)
+
+    if last_status == TaskProgressStatus.FAILED:
+        self.respond(
+            WebsocketResponse(
+                data={},
+                message="Invalid credentials",
+                status=WebsocketResponseStatus.ERROR,
+            )
+        )
+    elif last_status == TaskProgressStatus.COMPLETED:
+        self.respond(
+            WebsocketResponse(
+                data={
+                    "status": last_status,
+                    "result": task_progress[-1]["result"],
+                },
+                message="Destination connection check completed",
+                status=WebsocketResponseStatus.SUCCESS,
+            )
+        )
+    else:
+        self.respond(
+            WebsocketResponse(
+                data={},
+                message="No task found",
+                status=WebsocketResponseStatus.ERROR,
+            )
+        )
+    return
