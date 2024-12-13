25479787f2fa650a2c52715a31d3dd217b19ece4
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-12-13 18:18:21 +0530

    task keys should be random

--- ddpui/tests/websockets/test_airbyte_consumer.py
+++ ddpui/tests/websockets/test_airbyte_consumer.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from unittest.mock import Mock
 from ddpui.models.tasks import TaskProgressStatus
 from ddpui.utils.singletaskprogress import SingleTaskProgress
@@ -8,7 +9,7 @@ from ddpui.websockets.schemas import WebsocketResponse, WebsocketResponseStatus
 def test_polling_celery_no_stp():
     """tests polling_celery"""
     consumer = Mock(respond=Mock())
-    task_key = "test-task-key"
+    task_key = uuid4().hex
     polling_celery(consumer, task_key)
     consumer.respond.assert_called_once_with(
         WebsocketResponse(
@@ -20,7 +21,7 @@ def test_polling_celery_no_stp():
 def test_polling_celery_failed():
     """tests polling_celery"""
     consumer = Mock(respond=Mock())
-    task_key = "test-task-key"
+    task_key = uuid4().hex
     stp = SingleTaskProgress(task_key, 100)
     stp.add({"status": TaskProgressStatus.FAILED})
     polling_celery(consumer, task_key)
@@ -34,7 +35,7 @@ def test_polling_celery_failed():
 def test_polling_celery_succeeded():
     """tests polling_celery"""
     consumer = Mock(respond=Mock())
-    task_key = "test-task-key"
+    task_key = uuid4().hex
     stp = SingleTaskProgress(task_key, 100)
     stp.add({"status": TaskProgressStatus.COMPLETED, "result": "test-result"})
     polling_celery(consumer, task_key)
