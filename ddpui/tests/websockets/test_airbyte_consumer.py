6d70db48e8a703576698336ba4a29ec434c802a8
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-12-13 18:08:26 +0530

    tests

---
+++ ddpui/tests/websockets/test_airbyte_consumer.py
@@ -0,0 +1,47 @@
+from unittest.mock import Mock
+from ddpui.models.tasks import TaskProgressStatus
+from ddpui.utils.singletaskprogress import SingleTaskProgress
+from ddpui.websockets.airbyte_consumer import polling_celery
+from ddpui.websockets.schemas import WebsocketResponse, WebsocketResponseStatus
+
+
+def test_polling_celery_no_stp():
+    """tests polling_celery"""
+    consumer = Mock(respond=Mock())
+    task_key = "test-task-key"
+    polling_celery(consumer, task_key)
+    consumer.respond.assert_called_once_with(
+        WebsocketResponse(
+            data={}, message="No Task of this task_key found", status=WebsocketResponseStatus.ERROR
+        )
+    )
+
+
+def test_polling_celery_failed():
+    """tests polling_celery"""
+    consumer = Mock(respond=Mock())
+    task_key = "test-task-key"
+    stp = SingleTaskProgress(task_key, 100)
+    stp.add({"status": TaskProgressStatus.FAILED})
+    polling_celery(consumer, task_key)
+    consumer.respond.assert_called_once_with(
+        WebsocketResponse(
+            data={}, message="Invalid credentials", status=WebsocketResponseStatus.ERROR
+        )
+    )
+
+
+def test_polling_celery_succeeded():
+    """tests polling_celery"""
+    consumer = Mock(respond=Mock())
+    task_key = "test-task-key"
+    stp = SingleTaskProgress(task_key, 100)
+    stp.add({"status": TaskProgressStatus.COMPLETED, "result": "test-result"})
+    polling_celery(consumer, task_key)
+    consumer.respond.assert_called_once_with(
+        WebsocketResponse(
+            data={"status": TaskProgressStatus.COMPLETED, "result": "test-result"},
+            message="Successfully fetched source schema",
+            status=WebsocketResponseStatus.SUCCESS,
+        )
+    )
