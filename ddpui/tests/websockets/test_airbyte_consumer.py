64ed56f2d2d128d045fd69223403ab9b4e15954a
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-12-15 08:55:52 +0530

    more tests

--- ddpui/tests/websockets/test_airbyte_consumer.py
+++ ddpui/tests/websockets/test_airbyte_consumer.py
@@ -2,8 +2,40 @@ from uuid import uuid4
 from unittest.mock import Mock
 from ddpui.models.tasks import TaskProgressStatus
 from ddpui.utils.singletaskprogress import SingleTaskProgress
-from ddpui.websockets.airbyte_consumer import polling_celery
+from ddpui.websockets.airbyte_consumer import SchemaCatalogConsumer, polling_celery
 from ddpui.websockets.schemas import WebsocketResponse, WebsocketResponseStatus
+
+
+def test_websocket_receive_no_workspace():
+    """tests websocket_receive"""
+    consumer = SchemaCatalogConsumer()
+    consumer.connect = Mock()
+    consumer.respond = Mock()
+    consumer.orguser = Mock(org=Mock(airbyte_workspace_id=None))
+    consumer.websocket_receive({"text": "{}"})
+    consumer.respond.assert_called_once_with(
+        WebsocketResponse(
+            data={},
+            message="Create an airbyte workspace first.",
+            status=WebsocketResponseStatus.ERROR,
+        )
+    )
+
+
+def test_websocket_receive_no_source_id():
+    """tests websocket_receive"""
+    consumer = SchemaCatalogConsumer()
+    consumer.connect = Mock()
+    consumer.respond = Mock()
+    consumer.orguser = Mock(org=Mock(airbyte_workspace_id=1))
+    consumer.websocket_receive({"text": "{}"})
+    consumer.respond.assert_called_once_with(
+        WebsocketResponse(
+            data={},
+            message="SourceId is required in the payload",
+            status=WebsocketResponseStatus.ERROR,
+        )
+    )
 
 
 def test_polling_celery_no_stp():
