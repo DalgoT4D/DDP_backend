5422818801ba973032fedc584d77525b763efa24
Rohit Chatterjee <rohit@rootbridgeservices.in>
2024-12-17 19:33:40 +0530

    inline ssl certificate

--- ddpui/tests/core/datainsights/factories/test_warehouse_factory.py
+++ ddpui/tests/core/datainsights/factories/test_warehouse_factory.py
@@ -45,14 +45,121 @@ def test_warehouse_factory():
         WarehouseFactory.connect({}, "some-no-supported-warehouse-type")
 
 
-def test_url_encoding():
-    """tests url encoding of username and password"""
-
+def test_connect_args_1():
+    """tests creation on connect_args parameter to create_engine"""
     with patch("ddpui.datainsights.warehouse.postgres.inspect"):
         with patch("ddpui.datainsights.warehouse.postgres.create_engine") as mock_create_engine:
             PostgresClient(
-                {"username": "user name", "password": "pass word", "host": "host", "database": "db"}
+                {
+                    "username": "user name",
+                    "password": "pass word",
+                    "host": "host",
+                    "port": 1234,
+                    "database": "db",
+                    "sslrootcert": "sslrootcert",
+                    "sslmode": "require",
+                }
             )
             mock_create_engine.assert_called_with(
-                "postgresql://user%20name:pass%20word@host/db", pool_size=5, pool_timeout=30
+                "postgresql+psycopg2://",
+                connect_args={
+                    "host": "host",
+                    "port": 1234,
+                    "dbname": "db",
+                    "user": "user%20name",
+                    "password": "pass%20word",
+                    "sslrootcert": "sslrootcert",
+                    "sslmode": "require",
+                },
+                pool_size=5,
+                pool_timeout=30,
+            )
+
+
+def test_connect_args_2():
+    """tests creation on connect_args parameter to create_engine"""
+    with patch("ddpui.datainsights.warehouse.postgres.inspect"):
+        with patch("ddpui.datainsights.warehouse.postgres.create_engine") as mock_create_engine:
+            PostgresClient(
+                {
+                    "username": "user name",
+                    "password": "pass word",
+                    "host": "host",
+                    "port": 1234,
+                    "database": "db",
+                    "sslrootcert": "sslrootcert",
+                    "sslmode": True,
+                }
+            )
+            mock_create_engine.assert_called_with(
+                "postgresql+psycopg2://",
+                connect_args={
+                    "host": "host",
+                    "port": 1234,
+                    "dbname": "db",
+                    "user": "user%20name",
+                    "password": "pass%20word",
+                    "sslrootcert": "sslrootcert",
+                    "sslmode": "require",
+                },
+                pool_size=5,
+                pool_timeout=30,
+            )
+
+
+def test_connect_args_3():
+    """tests creation on connect_args parameter to create_engine"""
+    with patch("ddpui.datainsights.warehouse.postgres.inspect"):
+        with patch("ddpui.datainsights.warehouse.postgres.create_engine") as mock_create_engine:
+            PostgresClient(
+                {
+                    "username": "user name",
+                    "password": "pass word",
+                    "host": "host",
+                    "port": 1234,
+                    "database": "db",
+                    "sslrootcert": "sslrootcert",
+                    "sslmode": False,
+                }
+            )
+            mock_create_engine.assert_called_with(
+                "postgresql+psycopg2://",
+                connect_args={
+                    "host": "host",
+                    "port": 1234,
+                    "dbname": "db",
+                    "user": "user%20name",
+                    "password": "pass%20word",
+                    "sslrootcert": "sslrootcert",
+                    "sslmode": "disable",
+                },
+                pool_size=5,
+                pool_timeout=30,
+            )
+
+
+def test_url_encoding():
+    """tests url encoding of username and password"""
+    with patch("ddpui.datainsights.warehouse.postgres.inspect"):
+        with patch("ddpui.datainsights.warehouse.postgres.create_engine") as mock_create_engine:
+            PostgresClient(
+                {
+                    "username": "user name",
+                    "password": "pass word",
+                    "host": "host",
+                    "port": 1234,
+                    "database": "db",
+                }
+            )
+            mock_create_engine.assert_called_with(
+                "postgresql+psycopg2://",
+                connect_args={
+                    "host": "host",
+                    "port": 1234,
+                    "dbname": "db",
+                    "user": "user%20name",
+                    "password": "pass%20word",
+                },
+                pool_size=5,
+                pool_timeout=30,
             )
