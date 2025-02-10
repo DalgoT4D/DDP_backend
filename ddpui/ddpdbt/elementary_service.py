64883bcb4cbf8c60120cd0dd0e6263f245711622
Rohit Chatterjee <rohit@rootbridgeservices.in>
2025-02-10 21:01:29 +0530

    don't overwrite the elementary target schema

--- ddpui/ddpdbt/elementary_service.py
+++ ddpui/ddpdbt/elementary_service.py
@@ -236,13 +236,15 @@ def create_elementary_profile(org: Org):
         dbt_profile = yaml.safe_load(dbt_profile_file_f)
         logger.info("read dbt profile from %s", dbt_profile_file)
 
+    elementary_schema = elementary_profile["elementary"]["outputs"][target]["schema"]
+
     target = elementary_profile["elementary"].get("target", "default")
     elementary_profile["elementary"]["outputs"][target] = dbt_profile[dbt_profile_name]["outputs"][
         dbt_profiles_target
     ]
 
-    # set schema to elementary
-    elementary_profile["elementary"]["outputs"][target]["schema"] = "elementary"
+    # set schema to what the elementary.generate_elementary_cli_profile macro computed
+    elementary_profile["elementary"]["outputs"][target]["schema"] = elementary_schema
 
     elementary_profile_dir = Path(dbt_project_params.project_dir) / "elementary_profiles"
 
