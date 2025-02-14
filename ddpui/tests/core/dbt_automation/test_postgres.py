679d03db9a57ae0f15b20b9cc7963e39bb475075
Ishankoradia <ikoradia@umich.edu>
2025-02-14 21:04:21 +0530

    test cases fix

--- ddpui/tests/core/dbt_automation/test_postgres.py
+++ ddpui/tests/core/dbt_automation/test_postgres.py
@@ -4,7 +4,7 @@ from ddpui.dbt_automation.utils.postgres import PostgresClient
 
 def test_get_connection_1():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {"host": "HOST", "port": 1234, "user": "USER", "password": "PASSWORD"}
         )
@@ -19,7 +19,7 @@ def test_get_connection_1():
 
 def test_get_connection_2():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {
                 "host": "HOST",
@@ -41,7 +41,7 @@ def test_get_connection_2():
 
 def test_get_connection_3():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {
                 "sslmode": "verify-ca",
@@ -57,7 +57,7 @@ def test_get_connection_3():
 
 def test_get_connection_4():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {
                 "sslmode": True,
@@ -73,7 +73,7 @@ def test_get_connection_4():
 
 def test_get_connection_5():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {
                 "sslmode": False,
@@ -89,7 +89,7 @@ def test_get_connection_5():
 
 def test_get_connection_6():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {
                 "sslmode": {
@@ -105,7 +105,7 @@ def test_get_connection_6():
 
 def test_get_connection_7():
     """tests PostgresClient.get_connection"""
-    with patch("dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
+    with patch("ddpui.dbt_automation.utils.postgres.psycopg2.connect") as mock_connect:
         PostgresClient.get_connection(
             {"sslmode": {"mode": "disable", "ca_certificate": "LONG-CERTIFICATE"}}
         )
