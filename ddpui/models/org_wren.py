867a11feb880643306756a689565e89f4198ed0a
himanshudube97 <himanshu.dube13@gmail.com>
2025-03-04 08:29:13 +0530

    fixed the wren_url model

--- ddpui/models/org_wren.py
+++ ddpui/models/org_wren.py
@@ -7,6 +7,6 @@ class OrgWren(models.Model):
     """Model to store org's wrenai details"""
 
     org = models.OneToOneField(Org, on_delete=models.CASCADE, related_name="org_wren_info")
-    wren_url = models.CharField(max_length=255, blank=True, null=True)
+    wren_url = models.CharField(max_length=255, blank=False, null=False)
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(default=timezone.now)
