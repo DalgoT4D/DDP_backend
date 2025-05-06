a336508e41ae42d042765a8f5bfb149128913007
Rohit Chatterjee <rohit@rootbridgeservices.in>
2025-05-06 18:16:47 -0400

    removed [x] - prefect_service.schedule_deployment_flow_run [x] - airbyte_api.post_airbyte_connection_reset_v1 [x] - airbyte_api.update_connection_schema [x] - airbyte_service.reset_connection [x] - airbyte_service.update_schema_change [x] - airbytehelpers.reset_connection [x] - airbytehelpers.update_connection_schema [x] - deploymentblocks.py [x] - ConnectionMeta.schedule_large_jobs

--- ddpui/tests/utils/test_helpers.py
+++ ddpui/tests/utils/test_helpers.py
@@ -9,7 +9,6 @@ from ddpui.utils.helpers import (
     map_airbyte_keys_to_postgres_keys,
     update_dict_but_not_stars,
     nice_bytes,
-    get_schedule_time_for_large_jobs,
     find_key_in_dictionary,
 )
 
@@ -191,71 +190,6 @@ def test_nice_bytes():
     assert nice_bytes(1024) == "1.0 KB"
     assert nice_bytes(1024 * 1024) == "1.0 MB"
     assert nice_bytes(3 * 1024 * 1024) == "3.0 MB"
-
-
-def test_get_schedule_time_for_large_jobs_1():
-    """schedule for 10am UTC on the following sunday"""
-    r1 = get_schedule_time_for_large_jobs(datetime(2024, 1, 1))
-    assert r1.year == 2024
-    assert r1.month == 1
-    assert r1.day == 7
-    assert r1.hour == 10
-    assert r1.minute == 0
-    assert r1.tzinfo == pytz.utc
-
-
-def test_get_schedule_time_for_large_jobs_1_1():
-    """schedule for 10am UTC on the following sunday"""
-    r1 = get_schedule_time_for_large_jobs(datetime(2024, 1, 3))
-    assert r1.year == 2024
-    assert r1.month == 1
-    assert r1.day == 7
-    assert r1.hour == 10
-    assert r1.minute == 0
-    assert r1.tzinfo == pytz.utc
-
-
-def test_get_schedule_time_for_large_jobs_2():
-    """schedule for 10am UTC on the following sunday"""
-    r1 = get_schedule_time_for_large_jobs(datetime(2024, 1, 4, 13, 45))
-    assert r1.year == 2024
-    assert r1.month == 1
-    assert r1.day == 7
-    assert r1.hour == 10
-    assert r1.minute == 0
-    assert r1.tzinfo == pytz.utc
-
-
-def test_get_schedule_time_for_large_jobs_3():
-    """schedule for specified time UTC on the following sunday"""
-    r1 = get_schedule_time_for_large_jobs(datetime(2024, 1, 5, 13, 45), time(12, 30))
-    assert r1.year == 2024
-    assert r1.month == 1
-    assert r1.day == 7
-    assert r1.hour == 12
-    assert r1.minute == 30
-
-    assert r1.tzinfo == pytz.utc
-
-
-def test_get_schedule_time_for_large_jobs_4():
-    """if called on a sunday, schedule for the same sunday but an hour ahead"""
-    now = datetime(2024, 1, 7, 13, 45)
-    time_of_day = time(12, 30)
-    r1 = get_schedule_time_for_large_jobs(curr=now, time_of_day=time_of_day)
-    assert r1.year == now.year
-    assert r1.month == now.month
-    assert r1.day == now.day
-    assert r1.hour == now.hour + 1
-    assert r1.minute == now.minute
-    assert r1.tzinfo == pytz.utc
-
-
-def test_get_schedule_time_for_large_jobs_5():
-    """make sure the scheduled time is in the future"""
-    now = datetime(2024, 1, 7, 13, 45).astimezone(pytz.utc)
-    r1 = get_schedule_time_for_large_jobs(now, time(12, 30))
-    assert r1 >= now
 
 
 def test_find_key_in_dictionary():
