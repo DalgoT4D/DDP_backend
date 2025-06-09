b4f90f0f67240642d74b20079014e128ebf3b150
Rohit Chatterjee <rohit@rootbridgeservices.in>
2025-06-09 17:22:58 +0530

    add name to connectionmeta

---
+++ ddpui/management/commands/setconnectionnamesinmeta.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from ddpui.models.org import ConnectionMeta
+from ddpui.models.tasks import OrgTask
+from ddpui.ddpairbyte import airbyte_service
+
+
+class Command(BaseCommand):
+    """Pause & then resume scheduled deployments. This is a handy script when we see duplicate schedules or when we upgrade prefect"""
+
+    help = "Pause & then resume scheduled deployments. This is a handy script when we see duplicate schedules or when we upgrade prefect"
+
+    def add_arguments(self, parser):
+        """Docstring"""
+
+    def handle(self, *args, **options):
+        for orgtask in OrgTask.objects.filter(connection_id__isnull=False):
+            cm = ConnectionMeta.objects.filter(connection_id=orgtask.connection_id).first()
+            if cm is None:
+                cm = ConnectionMeta(connection_id=orgtask.connection_id)
+            if cm.connection_name is None:
+                airbyte_conn = airbyte_service.get_connection(
+                    orgtask.org.airbyte_workspace_id, orgtask.connection_id
+                )
+                cm.connection_name = airbyte_conn["name"]
+                cm.save()
