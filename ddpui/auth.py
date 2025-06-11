0e8a6b37e1138dee5fb8b3b3abaa26fe46e5a435
Ishankoradia <ikoradia@umich.edu>
2025-06-11 18:35:52 +0530

    cleanup the current auth file; we are not using any of it

--- ddpui/auth.py
+++ ddpui/auth.py
@@ -2,16 +2,12 @@ from functools import wraps
 from ninja.security import HttpBearer
 from ninja.errors import HttpError
 
-
-from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 
 from ddpui.models.org_user import OrgUser
-from ddpui.models.admin_user import AdminUser
 from ddpui.models.role_based_access import RolePermission
 from ddpui.utils import thread
 
-from ddpui.models.org_user import OrgUserRole
 
 UNAUTHORIZED = "unauthorized"
 
@@ -20,32 +16,6 @@ ACCOUNT_MANAGER_ROLE = "account-manager"
 PIPELINE_MANAGER_ROLE = "pipeline-manager"
 ANALYST_ROLE = "analyst"
 GUEST_ROLE = "guest"
-
-
-class BearerAuthentication(TokenAuthentication):
-    """
-    This allows us to send the Authorization header "Bearer <token>"
-    instead of "Token <token>"
-    """
-
-    keyword = "Bearer"
-
-
-class PlatformAdmin(HttpBearer):
-    """
-    ninja middleware to look up a user and an admin-user
-    (if it exists) from an auth token
-    """
-
-    def authenticate(self, request, token):
-        tokenrecord = Token.objects.filter(key=token).first()
-        if tokenrecord and tokenrecord.user:
-            request.user = tokenrecord.user
-            adminuser = AdminUser.objects.filter(user=request.user).first()
-            if adminuser is not None:
-                request.adminuser = adminuser
-                return tokenrecord
-        raise HttpError(400, UNAUTHORIZED)
 
 
 def has_permission(permission_slugs: list):
@@ -70,25 +40,6 @@ def has_permission(permission_slugs: list):
         return wrapper
 
     return decorator
-
-
-def authenticate_org_user(request, token, allowed_roles, require_org):
-    """docstring"""
-    tokenrecord = Token.objects.filter(key=token).first()
-    if tokenrecord and tokenrecord.user:
-        request.user = tokenrecord.user
-        q_orguser = OrgUser.objects.filter(user=request.user)
-        if request.headers.get("x-dalgo-org"):
-            orgslug = request.headers["x-dalgo-org"]
-            q_orguser = q_orguser.filter(org__slug=orgslug)
-        orguser = q_orguser.first()
-        if orguser is not None:
-            if require_org and orguser.org is None:
-                raise HttpError(400, "register an organization first")
-            if orguser.role in allowed_roles:
-                request.orguser = orguser
-                return request
-    raise HttpError(400, UNAUTHORIZED)
 
 
 class CustomAuthMiddleware(HttpBearer):
@@ -117,69 +68,3 @@ class CustomAuthMiddleware(HttpBearer):
                 return request
 
         raise HttpError(400, UNAUTHORIZED)
-
-
-class AnyOrgUser(HttpBearer):
-    """ninja middleware to allow any org user"""
-
-    def authenticate(self, request, token):
-        return authenticate_org_user(
-            request,
-            token,
-            [
-                OrgUserRole.REPORT_VIEWER,
-                OrgUserRole.PIPELINE_MANAGER,
-                OrgUserRole.ACCOUNT_MANAGER,
-            ],
-            False,
-        )
-
-
-class CanManagePipelines(HttpBearer):
-    """ninja middleware to allow only account-owners or pipeline-managers"""
-
-    def authenticate(self, request, token):
-        return authenticate_org_user(
-            request,
-            token,
-            [
-                OrgUserRole.PIPELINE_MANAGER,
-                OrgUserRole.ACCOUNT_MANAGER,
-            ],
-            True,
-        )
-
-
-class CanManageUsers(HttpBearer):
-    """
-    ninja middleware to look up a user and an org-user
-    (if it exists and is an account-owner or a pipeline-manager) from an auth token
-    """
-
-    def authenticate(self, request, token):
-        return authenticate_org_user(
-            request,
-            token,
-            [
-                OrgUserRole.PIPELINE_MANAGER,
-                OrgUserRole.ACCOUNT_MANAGER,
-            ],
-            True,
-        )
-
-
-class FullAccess(HttpBearer):
-    """
-    ninja middleware to look up a user and an org-user
-    (if it exists and is an account-owner) from an auth token
-    """
-
-    def authenticate(self, request, token):
-        return authenticate_org_user(
-            request,
-            token,
-            [
-                OrgUserRole.ACCOUNT_MANAGER,
-            ],
-            False,
-        )
