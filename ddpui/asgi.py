334c055ff338ae695cc29d5b33401273c4f33bf8
Ishankoradia <ikoradia@umich.edu>
2025-07-03 18:40:13 +0530

    looks like we need this middleware

--- ddpui/asgi.py
+++ ddpui/asgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ddpui.settings")
 
-django_asgi_app = get_asgi_application()
+django_asgi_app = OpenTelemetryMiddleware(get_asgi_application())
 
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
