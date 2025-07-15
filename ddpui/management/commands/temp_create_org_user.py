792fecd40693a83bfe8c2d2fb303ec8e521e7c8a
Siddhant Singh <ssiddhant3030@gmail.com>
2025-07-15 13:55:44 +0530

    new role fix

---
+++ ddpui/management/commands/temp_create_org_user.py
@@ -0,0 +1,93 @@
+import os
+import sys
+import getpass
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from ddpui.models.org import Org
+from ddpui.models.org_user import OrgUser, UserAttributes
+from ddpui.models.role_based_access import Role
+from ddpui.core.orgfunctions import create_organization
+from ddpui.models.org import OrgSchema
+
+
+class Command(BaseCommand):
+    """Creates an Org and an admin OrgUser"""
+
+    help = "Creates an Org and an OrgUser"
+
+    def add_arguments(self, parser):
+        """adds command line arguments"""
+        parser.add_argument("orgname", type=str, help="Name of the Org")
+        parser.add_argument("email", type=str, help="Email address of the OrgUser")
+        parser.add_argument(
+            "password",
+            type=str,
+            help="Password if creating a new User; can also supply via PASSWORD env var",
+            nargs="?",
+        )
+        parser.add_argument(
+            "--role",
+            type=str,
+            help="Role of the OrgUser; default is 'account-manager'",
+            default="account-manager",
+        )
+
+    def handle(self, *args, **options):
+        # ensure the Role exists
+        role = Role.objects.filter(slug=options["role"]).first()
+        if not role:
+            print(
+                f"Role with slug '{options['role']}' does not exist, please run 'python manage.py loaddata seed/*.json'"
+            )
+            sys.exit(1)
+
+        # fetch / create the Org
+        if not Org.objects.filter(name=options["orgname"]).exists():
+            create_organization(
+                OrgSchema(name=options["orgname"], slug=slugify(options["orgname"]))
+            )
+            print(f"Org {options['orgname']} created")
+        else:
+            print(f"Org {options['orgname']} already exists")
+        org = Org.objects.filter(name=options["orgname"]).first()
+
+        # fetch / create the User
+        if not User.objects.filter(email=options["email"]).exists():
+            password = options["password"]
+            if not password:
+                password = os.getenv("PASSWORD")
+            if not password:
+                password = getpass.getpass("Enter the password for the OrgUser: ")
+                password_confirm = getpass.getpass("Re-enter the password for the OrgUser: ")
+                if password != password_confirm:
+                    print("Passwords do not match")
+                    sys.exit(1)
+            if not password:
+                print("Password is required")
+                sys.exit(1)
+            User.objects.create_user(
+                email=options["email"], username=options["email"], password=password
+            )
+            print(f"User {options['email']} created")
+        else:
+            print(f"User {options['email']} already exists")
+        user = User.objects.filter(email=options["email"]).first()
+
+        if not OrgUser.objects.filter(org=org, user=user).exists():
+            OrgUser.objects.create(org=org, user=user, new_role=role, email_verified=True)
+            print(f"OrgUser {user.email} created for Org {org.name} with role {role.name}")
+        else:
+            print(f"OrgUser {user.email} already exists for Org {org.name}")
+        orguser = OrgUser.objects.filter(org=org, user=user).first()
+
+        # ensure the UserAttributes exists with email_verified=True
+        if not UserAttributes.objects.filter(user=user).exists():
+            UserAttributes.objects.create(user=user)
+            print("UserAttributes created for User")
+        ua = UserAttributes.objects.filter(user=user).first()
+        ua.email_verified = True
+        ua.can_create_orgs = True
+        ua.save()
+        print(f"UserAttributes updated for User {user.email} with email_verified=True")
--- ddpui/settings.py
+++ ddpui/settings.py
@@ -23,16 +23,16 @@ import ddpui.utils.flags  # pylint: disable=unused-import
 
 load_dotenv()
 
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=float(os.getenv("SENTRY_TSR", "1.0")),
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=float(os.getenv("SENTRY_PSR", "1.0")),
-)
+# sentry_sdk.init(
+#     dsn=os.getenv("SENTRY_DSN"),
+#     # Set traces_sample_rate to 1.0 to capture 100%
+#     # of transactions for performance monitoring.
+#     traces_sample_rate=float(os.getenv("SENTRY_TSR", "1.0")),
+#     # Set profiles_sample_rate to 1.0 to profile 100%
+#     # of sampled transactions.
+#     # We recommend adjusting this value in production.
+#     profiles_sample_rate=float(os.getenv("SENTRY_PSR", "1.0")),
+# )
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
