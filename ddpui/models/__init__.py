6559f51d44e55a0fbecedc479520a9c514d74783
Ishankoradia <ikoradia@umich.edu>
2025-07-31 13:32:24 +0530

    cleanups

--- ddpui/models/__init__.py
+++ ddpui/models/__init__.py
@@ -4,6 +4,6 @@ from ddpui.models.airbyte import SyncStats, AirbyteJob
 from ddpui.models.org_preferences import OrgPreferences
 from ddpui.models.org_supersets import OrgSupersets
 from ddpui.models.org_wren import OrgWren
-from ddpui.models.visualization import Chart, Metric
+from ddpui.models.visualization import Chart
 from ddpui.models.admin_user import AdminUser
 from ddpui.models.visualization import Chart
