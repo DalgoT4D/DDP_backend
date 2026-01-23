87640052d7b3092e6aa044c83e248be92be9f771
Ishankoradia <ikoradia@umich.edu>
2026-01-23 17:47:04 +0530

    minor changes

--- ddpui/utils/custom_logger.py
+++ ddpui/utils/custom_logger.py
@@ -5,5 +5,5 @@ from ddpui.utils.unified_logger import DalgoLogger
 class CustomLogger(DalgoLogger):
     """Backward compatible CustomLogger that now uses unified logging"""
 
-    def __init__(self, name, level=logging.INFO):
-        super().__init__(name, level)
+    def __init__(self, name):
+        super().__init__(name)
