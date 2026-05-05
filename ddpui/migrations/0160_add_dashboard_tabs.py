b1708fd174d40cbc92ca8df348ef3b7040f5cbb9
Naveen Kumar <73029naveenkumar73537@gmail.com>
2026-05-05 16:33:40 +0530

    Add 'tabs' field to dashboard model for enhanced tab management

--- ddpui/migrations/0160_add_dashboard_tabs.py
+++ ddpui/migrations/0160_add_dashboard_tabs.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("ddpui", "0152_reportsnapshot"),
+        ("ddpui", "0156_add_comment_snapshot_index"),
     ]
 
     operations = [
