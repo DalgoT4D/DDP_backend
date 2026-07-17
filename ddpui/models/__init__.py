e15f44d332382472e08e0358ea3af8bcef41cc4f
himanshudube97 <himanshu.dube13@gmail.com>
2026-07-17 20:17:21 +0530

    feat(trial): TrialClone state-machine + timings/manifest model

--- ddpui/models/__init__.py
+++ ddpui/models/__init__.py
@@ -13,3 +13,4 @@ from ddpui.models.canvas_models import CanvasNode, CanvasEdge
 from ddpui.models.comment import Comment, CommentReadStatus
 from ddpui.models.metric import Metric, KPI
 from ddpui.models.alert import Alert, AlertLog, AlertType
+from ddpui.models.trial_clone import TrialClone, TrialCloneStatus
