4f4fb1dd4d590ca3841a7ada73492d478c46ac37
Ishankoradia <ikoradia@umich.edu>
2026-09-25 13:33:11 +0530

    updates

--- ddpui/services/org_cleanup_service.py
+++ ddpui/services/org_cleanup_service.py
@@ -14,7 +14,6 @@ from ddpui.models.org_plans import OrgPlans
 from ddpui.models.org_preferences import OrgPreferences
 from ddpui.models.llm import LlmSession
 from ddpui.models.metric import KPI, Metric
-from rest_framework.authtoken.models import Token
 
 from ddpui.ddpairbyte import airbyte_service
 from ddpui.ddpprefect import prefect_service
@@ -391,7 +390,10 @@ class OrgCleanupService:
                 remaining = OrgUser.objects.filter(user=user).exclude(org=self.org).count()
                 if remaining == 0:
                     logger.info("deleting user %s (no remaining org memberships)", user.email)
-                    Token.objects.filter(user=user).delete()
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            "DELETE FROM authtoken_token WHERE user_id = %s", [user.pk]
+                        )
                     user.delete()
                 else:
                     logger.info(
